c52b4b953c40aa863fa7e0de28fb74d14682454a
Randy McMillan <randy.lee.mcmillan@gmail.com>
2015-12-19 20:41:26 -0500

    ..

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,30 +3,35 @@ set -e
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -avv --progress . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	read -p "Installs QuickVim [https://github.com/randymcmillan/QuickVim.git] 
+    as well as the dotfiles [https://github.com/randymcmillan/dotfiles.git] files.
+    This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
-	fi
+
 if [ -d ~/QuickVim ]
 		then
 				echo '~/QuickVim exists'
                 cd ~/QuickVim
-                ~/QuickVim/./quick-vim install
+                ~/QuickVim/./quick-vim upgrade
 		else
-				echo 'cloning ~/QuickVim'
+				echo 'cloning
+                https://github.com/randymcmillan/QuickVim.git to ~/QuickVim'
 				git clone https://github.com/randymcmillan/QuickVim.git ~/QuickVim
 				cd ~/QuickVim
-				echo $PWD
-                ~/QuickVim/./quick-vim upgrade
+                ~/QuickVim/./quick-vim install
 
 		fi
+
+	fi
 unset doIt
+cd ~/
 source ~/.bash_profile
 source ~/.aliases
 fi

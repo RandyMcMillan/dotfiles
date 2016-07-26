27be3651c6eecaf5478fbffdf5c6f2e3474364f5
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-07-25 20:03:06 -0400

    git add * ; uncrustify; git commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,32 +7,36 @@ function doIt() {
 }
 #https://github.com/altercation/solarized.git
 function installSolarized() {
-            if [ -d ~/solarized ]
+            if [ -d ~/solarized/.git ]
               then
                       echo '~/solarized exists'
-                      cd ~/solarized/
+                      cd ~/solarized
                        git pull
               else
+rm -rf ~/solarized
                       cd ~/
                       echo 'cloning https://github.com/altercation/solarized.git to ~/solarized'
                       git clone https://github.com/altercation/solarized.git
                     fi
 }
 function installQuickVim() {
-            if [ -d ~/QuickVim ]
+            if [ -d ~/QuickVim/.git ]
               then
                       echo '~/QuickVim exists'
                       cd ~/QuickVim/
+git pull
                        ~/QuickVim/./quick-vim upgrade
               else
+rm -rf ~/QuickVim
                       echo 'cloning https://github.com/randymcmillan/QuickVim.git to ~/QuickVim'
+cd ~/
                       git clone https://github.com/randymcmillan/QuickVim.git
                       cd ~/QuickVim/
                       ~/QuickVim/./quick-vim install
                     fi
 }
 function installISightCapture() {
-            if [ -d ~/iSightCapture ]
+            if [ -d ~/iSightCapture/.git ]
               then
                   echo '~/iSightCapture exists'
                   cd ~/iSightCapture/
@@ -40,18 +44,21 @@ function installISightCapture() {
               else
                   echo 'cloning
                   https://github.com/randymcmillan/iSightCapture.git
-                  to ~/iSIghtCapture'
+                  to ~/iiIghtCapture'
+cd ~/
                   git clone
                   https://github.com/randymcmillan/iSightCapture.git
               fi
 }
 function installMyUncrustifyConfigs() {
-            if [ -d ~/my-uncrustify-configs ]
+            if [ -d ~/my-uncrustify-configs/.git ]
                 then
-                    echo '~/my-uncrustify-configs'
+                    echo '~/my-uncrustify-configs exists'
                     cd ~/my-uncrustify-configs
                     git pull
                 else
+rm -rf ~/my-uncrustify-configs
+cd ~/
                     echo 'cloning
                     https://github.com/RandyMcMillan/my-uncrustify-configs.git to
                     ~/my-uncrustify-configs'
@@ -60,29 +67,31 @@ function installMyUncrustifyConfigs() {
 }
 #https://gist.github.com/f4c040ce48c0d8e590c125379e1ef69b.git
 function installRecursiveIndex(){
-						 if [ -d ~/f4c040ce48c0d8e590c125379e1ef69b ]
+						 if [ -d ~/f4c040ce48c0d8e590c125379e1ef69b/.git ]
 						   then
-						       echo '~/f4c040ce48c0d8e590c125379e1ef69b/ git pull'
+						       echo '~/f4c040ce48c0d8e590c125379e1ef69b/ exists'
 						       cd ~/f4c040ce48c0d8e590c125379e1ef69b
 						       git pull
 						   else
+rm -rf ~/f4c040ce48c0d8e590c125379e1ef69b
 									cd ~/
          echo 'cloning https://gist.github.com/f4c040ce48c0d8e590c125379e1ef69b.git to ~/f4c040ce48c0d8e590c125379e1ef69b'
-            git clone https://gist.github.com/f4c040ce48c0d8e590c125379e1ef69b.git ~/f4c040ce48c0d8e590c125379e1ef69b
+            git clone https://gist.github.com/f4c040ce48c0d8e590c125379e1ef69b.git
 						fi
 }
 #Keep this last
 #git@github.com:lindes/get-location.git
 function installGetLocation(){
-						 if [ -d ~/get-location ]
+						 if [ -d ~/get-location/.git ]
 						   then
-						       echo '~/get-location/ git pull'
+						       echo '~/get-location/ exists'
 						       cd ~/get-location
 						       git pull
 						   else
+rm -rf ~/get-location
 									cd ~/
 									 echo 'cloning https://github.com/RandyMcMillan/get-location.git to ~/get-location'
-						        git clone https://github.com/lindes/get-location.git ~/get-location
+						        git clone https://github.com/lindes/get-location.git
 						fi
 						cd ~/get-location/
 						sudo make
@@ -99,13 +108,13 @@ else
 		doIt
         installSolarized
         installQuickVim
-        #installISightCapture
+        installISightCapture
         installMyUncrustifyConfigs
 				installRecursiveIndex
 				installGetLocation
 		unset doIt
 		unset installQuickVim
-		#unset installISightCapture
+		unset installISightCapture
 		unset installMyUncrustifyConfigs
 		unset installGetLocation
 		unset installRecursiveIndex
@@ -114,5 +123,7 @@ else
 		source ~/.aliases
 		source ~/.bashrc
 		source ~/.osx
+source ~/.extra
+source ~/.functions
     fi
 fi

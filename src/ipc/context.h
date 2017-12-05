852f936ae10bed89065ef71c5c32368c0a8b2da6
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    ipc # Add ipc::Context and ipc::capnp::Context structs
    Add ipc::Context and ipc::capnp::Context structs
    
    These are currently empty structs but they will be used to pass some
    function and object pointers non-ipc code to ipc-code.

---
+++ src/ipc/context.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_IPC_CONTEXT_H
+#define BITCOIN_IPC_CONTEXT_H
+
+namespace ipc {
+//! Context to give IPC protocol hooks access to application state.
+struct Context
+{
+};
+} // namespace ipc
+
+#endif // BITCOIN_IPC_CONTEXT_H

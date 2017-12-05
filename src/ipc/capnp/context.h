9a7b3db9a29c5e3b00a4cc069ef87e1bb9404f35
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    Make bitcoin-gui spawn a bitcoin-node process
    Spawn node subprocess instead of running node code internally

--- src/ipc/capnp/context.h
+++ src/ipc/capnp/context.h
@@ -5,12 +5,26 @@
 #ifndef BITCOIN_IPC_CAPNP_CONTEXT_H
 #define BITCOIN_IPC_CAPNP_CONTEXT_H
 
+#include <ipc/capnp/node.capnp.h>
 #include <ipc/context.h>
+
+namespace interfaces {
+class Node;
+} // namespace interfaces
+namespace mp {
+struct InvokeContext;
+} // namespace mp
 
 namespace ipc {
 namespace capnp {
 struct Context : ipc::Context
 {
+    using MakeNodeClient = std::unique_ptr<interfaces::Node>(mp::InvokeContext& context,
+                                                             messages::Node::Client&& client);
+    using MakeNodeServer = kj::Own<messages::Node::Server>(mp::InvokeContext& context,
+                                                           std::shared_ptr<interfaces::Node> impl);
+    MakeNodeClient* make_node_client = nullptr;
+    MakeNodeServer* make_node_server = nullptr;
 };
 } // namespace capnp
 } // namespace ipc

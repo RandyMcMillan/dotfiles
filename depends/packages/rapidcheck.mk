ca0dbbd64fba9d31d0ff88984529deb4b87229fc
Russell Yanofsky <russ@yanofsky.org>
2018-02-09 12:41:44 -0500

    autotools/depends fixes for rapidcheck
    Fixes non-depends bitcoin build when rapidcheck is not installed on system,
    by adding --with-rapidcheck config option.
    
    Fixes depends build by adding fPIC option and installation steps so the build
    is not empty. Allows property-based tests to be run with:
    
        make -C depends
        ./configure --prefix=$PWD/depends/x86_64-pc-linux-gnu
        make check

--- depends/packages/rapidcheck.mk
+++ depends/packages/rapidcheck.mk
@@ -5,9 +5,14 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=78cdb8d0185b602e32e66f4e5d1a6ceec1f801dd9641b8a9456c386b1eaaf0e5
 
 define $(package)_config_cmds
-  cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix)
+  cmake -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true .
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) && \
+  mkdir -p $($(package)_staging_dir)$(host_prefix)/include && \
+  cp -a include/* $($(package)_staging_dir)$(host_prefix)/include/ && \
+  cp -a extras/boost_test/include/rapidcheck/* $($(package)_staging_dir)$(host_prefix)/include/rapidcheck/ && \
+  mkdir -p $($(package)_staging_dir)$(host_prefix)/lib && \
+  cp -a librapidcheck.a $($(package)_staging_dir)$(host_prefix)/lib/
 endef

9d1735d3a09be71913476685d52164aac3971f28
Russell Yanofsky <russ@yanofsky.org>
2018-08-23 13:42:31 -0400

    multiprocess: Add -ipcconnect and -ipcbind options
    Add `-ipcbind` option to `bitcoin-node`, and an `-ipcconnect` option to
    `bitcoin-wallet` to allow running a node that listens on an IPC socket and
    accepts connections from wallet processes.
    
    Example usage:
    
        src/bitcoin-node -regtest -debug -ipcbind=unix
        src/bitcoin-wallet -regtest -ipcconnect=unix info
    
    `bitcoin-wallet` tool doesn't really do anything with its connection to the
    node yet, but it could potentially run or serve RPCs that require being online.

--- src/interfaces/init.cpp
+++ src/interfaces/init.cpp
@@ -12,6 +12,8 @@
 #include <logging.h>
 #include <util/memory.h>
 
+#include <tinyformat.h>
+
 namespace interfaces {
 namespace {
 //! Close hook that encapsulates a function to be called on close.
@@ -53,5 +55,26 @@ void SpawnProcess(IpcProcess& process,
         LogPrint(::BCLog::IPC, "%s pid %i exited with status %i\n", new_exe_name, pid, status);
     }));
     base.addCloseHook(MakeUnique<Deleter<std::unique_ptr<Init>>>(std::move(init)));
+}
+
+bool ConnectAddress(IpcProcess& process, IpcProtocol& protocol, const fs::path& data_dir, std::string& address, const MakeClientFn& make_client)
+{
+    if (address.empty() || address == "0") return false;
+    int fd = -1;
+    std::string error;
+    if (address == "auto") {
+        // failure to connect with "auto" isn't an error. Caller can spawn a child process or just work offline.
+        address = "unix";
+        fd = process.connect(data_dir, "bitcoin-node", address, error);
+        if (fd < 0) return false;
+    } else {
+        fd = process.connect(data_dir, "bitcoin-node", address, error);
+    }
+    if (fd < 0) {
+        throw std::runtime_error(strprintf("Could not connect to bitcoin-node IPC address '%s'. %s", address, error));
+    }
+    std::unique_ptr<Init> init = protocol.connect(fd);
+    make_client(*init).addCloseHook(MakeUnique<Deleter<std::unique_ptr<Init>>>(std::move(init)));
+    return true;
 }
 } // namespace interfaces

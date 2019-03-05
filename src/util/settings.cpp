ebc33836dd8a116b4ea71cfd60cee32396721433
Russell Yanofsky <russ@yanofsky.org>
2019-03-05 16:07:48 -0500

    Add util::Settings accessor functions.
    Implement merging of settings from different sources (command line and config
    file) separately from parsing code in system.cpp, so it is easier to add new
    sources.
    
    Document current inconsistent merging behavior without changing it.
    
    This commit only adds new settings code without using it. The next commit calls
    the new code to replace existing code in system.cpp.

--- src/util/settings.cpp
+++ src/util/settings.cpp
@@ -7,10 +7,157 @@
 #include <univalue.h>
 
 namespace util {
+namespace {
+
+struct Source {
+    SettingsSpan span;
+    bool forced = false;
+    bool config_file = false;
+    bool config_file_default_section = false;
+
+    explicit Source(SettingsSpan span) : span(span) {}
+    Source& SetForced() noexcept { forced = true; return *this; }
+    Source& SetConfigFile(bool default_section) noexcept { config_file = true; config_file_default_section = default_section; return *this; }
+};
+
+template <typename Fn>
+static void MergeSettings(const Settings& settings, const std::string& section, const std::string& name, Fn&& fn)
+{
+    if (auto* value = FindKey(settings.forced_settings, name)) {
+        fn(Source(SettingsSpan(*value)).SetForced());
+    }
+    if (auto* values = FindKey(settings.command_line_options, name)) {
+        fn(Source(SettingsSpan(*values)));
+    }
+    if (!section.empty()) {
+        if (auto* map = FindKey(settings.ro_config, section)) {
+            if (auto* values = FindKey(*map, name)) {
+                fn(Source(SettingsSpan(*values)).SetConfigFile(/* default_section= */ false));
+            }
+        }
+    }
+    if (auto* map = FindKey(settings.ro_config, "")) {
+        if (auto* values = FindKey(*map, name)) {
+            fn(Source(SettingsSpan(*values)).SetConfigFile(/* default_section= */ true));
+        }
+    }
+}
+} // namespace
+
+SettingsValue GetSetting(const Settings& settings,
+    const std::string& section,
+    const std::string& name,
+    bool ignore_default_section_config,
+    bool get_chain_name)
+{
+    SettingsValue result;
+    MergeSettings(settings, section, name, [&](Source source) {
+        // Weird behavior preserved for backwards compatibility: Apply negated
+        // setting in otherwise ignored sections. A negated value in the
+        // default section is applied to network specific options, even though
+        // non-negated values there would be ignored.
+        const bool never_ignore_negated_setting = source.span.last_negated();
+
+        // Weird behavior preserved for backwards compatibility: Take first
+        // assigned value instead of last. In general, later settings take
+        // precedence over early settings, but for backwards compatibility in
+        // the config file the precedence is reversed for most settings.
+        const bool reverse_precedence = source.config_file && !get_chain_name;
+
+        // Weird behavior preserved for backwards compatibility: Negated
+        // -regtest and -testnet arguments which you would expect to override
+        // values set in the configuration file are currently accepted but
+        // silently ignored. It would be better to apply these just like other
+        // negated values, or at least warn they are ignored.
+        const bool skip_negated_command_line = get_chain_name;
+
+        // Ignore settings in default config section if requested.
+        if (ignore_default_section_config && source.config_file_default_section && !never_ignore_negated_setting) return;
+
+        // Skip negated command line settings.
+        if (skip_negated_command_line && source.span.last_negated()) return;
+
+        // Stick with highest priority value, keeping result if already set.
+        if (!result.isNull()) return;
+
+        if (!source.span.empty()) {
+            result = reverse_precedence ? source.span.begin()[0] : source.span.end()[-1];
+        } else if (source.span.last_negated()) {
+            result = false;
+        }
+    });
+    return result;
+}
+
+std::vector<SettingsValue> GetSettingsList(const Settings& settings,
+    const std::string& section,
+    const std::string& name,
+    bool ignore_default_section_config)
+{
+    std::vector<SettingsValue> result;
+    bool result_complete = false;
+    bool prev_negated_empty = false;
+    MergeSettings(settings, section, name, [&](Source source) {
+        // Weird behavior preserved for backwards compatibility: Apply config
+        // file settings even if negated on command line. Negating a setting on
+        // command line will discard earlier settings on the command line and
+        // settings in the config file, unless the negated command line value is
+        // followed by non-negated value, in which case config file settings
+        // will be brought back from the dead (but earlier command line settings
+        // will still be discarded).
+        const bool add_zombie_config_values = source.config_file && !prev_negated_empty;
+
+        // Ignore settings in default config section if requested.
+        if (ignore_default_section_config && source.config_file_default_section) return;
+
+        // Add new settings to the result if isn't already complete, or if the
+        // values are zombies.
+        if (!result_complete || add_zombie_config_values) {
+            for (const auto& value : source.span) {
+                if (value.isArray()) {
+                    result.insert(result.end(), value.getValues().begin(), value.getValues().end());
+                } else {
+                    result.push_back(value);
+                }
+            }
+        }
+
+        // If a setting was negated, or if a setting was forced, set
+        // result_complete to true to ignore any later lower priority settings.
+        result_complete |= source.span.negated() > 0 || source.forced;
+
+        // Update the negated and empty state used for the zombie values check.
+        prev_negated_empty |= source.span.last_negated() && result.empty();
+    });
+    return result;
+}
+
+bool OnlyHasDefaultSectionSetting(const Settings& settings, const std::string& section, const std::string& name)
+{
+    bool has_default_section_setting = false;
+    bool has_other_setting = false;
+    MergeSettings(settings, section, name, [&](Source source) {
+        if (source.span.empty()) return;
+        else if (source.config_file_default_section) has_default_section_setting = true;
+        else has_other_setting = true;
+    });
+    // If a value is set in the default section and not explicitly overwritten by the
+    // user on the command line or in a different section, then we want to enable
+    // warnings about the value being ignored.
+    return has_default_section_setting && !has_other_setting;
+}
 
 SettingsSpan::SettingsSpan(const std::vector<SettingsValue>& vec) noexcept : SettingsSpan(vec.data(), vec.size()) {}
-const SettingsValue* SettingsSpan::begin() const { return data; }
+const SettingsValue* SettingsSpan::begin() const { return data + negated(); }
 const SettingsValue* SettingsSpan::end() const { return data + size; }
-bool SettingsSpan::empty() const { return size == 0; }
+bool SettingsSpan::empty() const { return size == 0 || last_negated(); }
+bool SettingsSpan::last_negated() const { return size > 0 && data[size - 1].isFalse(); }
+size_t SettingsSpan::negated() const
+{
+    for (size_t i = size; i > 0; --i) {
+        if (data[i - 1].isFalse()) return i; // Return number of negated values (position of last false value)
+    }
+    return 0;
+}
 
 } // namespace util
--- src/util/settings.h
+++ src/util/settings.h
@@ -27,10 +27,51 @@ using SettingsValue = UniValue;
 //! Stored bitcoin settings. This struct combines settings from the command line
 //! and a read-only configuration file.
 struct Settings {
+    //! Map of setting name to forced setting value.
+    std::map<std::string, SettingsValue> forced_settings;
     //! Map of setting name to list of command line values.
     std::map<std::string, std::vector<SettingsValue>> command_line_options;
     //! Map of config section name and setting name to list of config file values.
     std::map<std::string, std::map<std::string, std::vector<SettingsValue>>> ro_config;
+};
+
+//! Get settings value from combined sources: forced settings, command line
+//! arguments and the read-only config file.
+//!
+//! @param ignore_default_section_config - ignore values in the default section
+//!                                        of the config file (part before any
+//!                                        [section] keywords)
+//! @param get_chain_name - enable special backwards compatible behavior
+//!                         for GetChainName
+SettingsValue GetSetting(const Settings& settings, const std::string& section, const std::string& name, bool ignore_default_section_config, bool get_chain_name);
+
+//! Get combined setting value similar to GetSetting(), except if setting was
+//! specified multiple times, return a list of all the values specified.
+std::vector<SettingsValue> GetSettingsList(const Settings& settings, const std::string& section, const std::string& name, bool ignore_default_section_config);
+
+//! Return true if a setting is set in the default config file section, and not
+//! overridden by a higher priority command-line or network section value.
+//!
+//! This is used to provide user warnings about values that might be getting
+//! ignored unintentionally.
+bool OnlyHasDefaultSectionSetting(const Settings& settings, const std::string& section, const std::string& name);
+
+//! Accessor for list of settings that skips negated values when iterated over.
+//! The last boolean `false` value in the list and all earlier values are
+//! considered negated.
+struct SettingsSpan {
+    explicit SettingsSpan() = default;
+    explicit SettingsSpan(const SettingsValue& value) noexcept : SettingsSpan(&value, 1) {}
+    explicit SettingsSpan(const SettingsValue* data, size_t size) noexcept : data(data), size(size) {}
+    explicit SettingsSpan(const std::vector<SettingsValue>& vec) noexcept;
+    const SettingsValue* begin() const; //<! Pointer to first non-negated value.
+    const SettingsValue* end() const;   //<! Pointer to end of values.
+    bool empty() const;                 //<! True if there are any non-negated values.
+    bool last_negated() const;          //<! True if the last value is negated.
+    size_t negated() const;             //<! Number of negated values.
+
+    const SettingsValue* data = nullptr;
+    size_t size = 0;
 };
 
 //! Map lookup helper.
@@ -40,20 +81,6 @@ auto FindKey(Map&& map, Key&& key) -> decltype(&map.at(key))
     auto it = map.find(key);
     return it == map.end() ? nullptr : &it->second;
 }
-
-//! Accessor for list of settings.
-struct SettingsSpan {
-    explicit SettingsSpan() = default;
-    explicit SettingsSpan(const SettingsValue& value) noexcept : SettingsSpan(&value, 1) {}
-    explicit SettingsSpan(const SettingsValue* data, size_t size) noexcept : data(data), size(size) {}
-    explicit SettingsSpan(const std::vector<SettingsValue>& vec) noexcept;
-    const SettingsValue* begin() const;
-    const SettingsValue* end() const;
-    bool empty() const;
-
-    const SettingsValue* data = nullptr;
-    size_t size = 0;
-};
 
 } // namespace util
 

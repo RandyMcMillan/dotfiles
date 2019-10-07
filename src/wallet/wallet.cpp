0c823e4d5c687fe116d2de550888558a3005b184
Andrew Chow <achow101-github@achow101.com>
2019-10-07 14:11:34 -0400

    Key pool: Make TopUp fail if unexpected wallet flags are set
    This adds a sanity check, and doesn't appear to effect existing calls.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3044,6 +3044,9 @@ unsigned int CWallet::GetKeyPoolSize() const
 bool CWallet::TopUpKeyPool(unsigned int kpSize)
 {
     LOCK(cs_wallet);
+    if (IsWalletFlagSet(WALLET_FLAG_BLANK_WALLET) || IsWalletFlagSet(WALLET_FLAG_DISABLE_PRIVATE_KEYS)) {
+        return false;
+    }
     bool res = true;
     if (auto spk_man = m_spk_man.get()) {
         res &= spk_man->TopUp(kpSize);

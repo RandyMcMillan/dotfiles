157dd6b15de5e48df65e8cad14dcdd5a1a28405a
Russell Yanofsky <russ@yanofsky.org>
2019-11-15 16:26:23 -0500

    util: Forbid ambiguous multiple assignments in config file
    Enable error "Multiple values specified for -setting in same section of config
    file.", for ALLOW_ANY settings that don't specify ALLOW_LIST.
    
    Instead of silently ignoring settings, this change makes it an error to provide
    an ambiguous config file that provides assigns multiple values to a
    single-value setting. Change include release notes.

--- doc/release-notes.md
+++ doc/release-notes.md
@@ -350,6 +350,15 @@ Tests
   setting. The settings `-signetchallenge` and `-signetseednode` allow
   enabling a custom signet.
 
+Configuration file
+------------------
+
+Repeated assignments of the same setting in the same section of the config file
+which were previously ignored will now trigger errors on startup that look
+like: "Multiple values specified for -setting in same section of config file."
+Settings like `-debug=<category>` and `-rpcallowip=<addr>` which are meant to
+be repeated are not affected by this change and will not trigger errors.
+
 Credits
 =======
 
--- src/util/system.cpp
+++ src/util/system.cpp
@@ -896,7 +896,7 @@ bool ArgsManager::ReadConfigStream(std::istream& stream, const std::string& file
         bool negated = !InterpretKey(section, key);
         Optional<unsigned int> flags = GetArgFlags('-' + key);
         if (flags) {
-            if (!(*flags & (ALLOW_ANY | ALLOW_LIST)) && m_settings.ro_config[section].count(key)) {
+            if (!(*flags & ALLOW_LIST) && m_settings.ro_config[section].count(key)) {
                 error = strprintf("Multiple values specified for -%s in same section of config file.", key);
                 return false;
             }

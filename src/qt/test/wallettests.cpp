5dea35fefaca612a04206e863a10fdb38c3df1c9
Russell Yanofsky <russ@yanofsky.org>
2020-01-16 16:13:15 -0500

    wallet refactor: Avoid use of Chain::Lock in qt wallettests
    This is a step toward removing the Chain::Lock class and reducing cs_main
    locking.
    
    This change doesn't affect behavior.

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -156,7 +156,7 @@ void TestGUI(interfaces::Node& node)
 
         WalletRescanReserver reserver(wallet.get());
         reserver.reserve();
-        CWallet::ScanResult result = wallet->ScanForWalletTransactions(locked_chain->getBlockHash(0), {} /* stop_block */, reserver, true /* fUpdate */);
+        CWallet::ScanResult result = wallet->ScanForWalletTransactions(Params().GetConsensus().hashGenesisBlock, {} /* stop_block */, reserver, true /* fUpdate */);
         QCOMPARE(result.status, CWallet::ScanResult::SUCCESS);
         QCOMPARE(result.last_scanned_block, ::ChainActive().Tip()->GetBlockHash());
         QVERIFY(result.last_failed_block.IsNull());

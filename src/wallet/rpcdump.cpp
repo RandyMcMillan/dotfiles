d4bc495a9c74542f4955cb73e2f2f89424d15ffd
Russell Yanofsky <russ@yanofsky.org>
2020-01-16 16:42:01 -0500

    wallet: Avoid use of Chain::Lock in importwallet and dumpwallet
    This is a step toward removing the Chain::Lock class and reducing cs_main
    locking.
    
    This change only affects behavior in the case where wallet last block processed
    falls behind the chain tip, in which case it will use more accurate backup and
    rescan timestamps.

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -564,8 +564,7 @@ UniValue importwallet(const JSONRPCRequest& request)
         if (!file.is_open()) {
             throw JSONRPCError(RPC_INVALID_PARAMETER, "Cannot open wallet dump file");
         }
-        Optional<int> tip_height = locked_chain->getHeight();
-        nTimeBegin = tip_height ? locked_chain->getBlockTime(*tip_height) : 0;
+        pwallet->chain().findBlock(pwallet->GetLastBlockHash(), nullptr, &nTimeBegin);
 
         int64_t nFilesize = std::max((int64_t)1, (int64_t)file.tellg());
         file.seekg(0, file.beg);
@@ -789,9 +788,10 @@ UniValue dumpwallet(const JSONRPCRequest& request)
     // produce output
     file << strprintf("# Wallet dump created by Bitcoin %s\n", CLIENT_BUILD);
     file << strprintf("# * Created on %s\n", FormatISO8601DateTime(GetTime()));
-    const Optional<int> tip_height = locked_chain->getHeight();
-    file << strprintf("# * Best block at time of backup was %i (%s),\n", tip_height.get_value_or(-1), tip_height ? locked_chain->getBlockHash(*tip_height).ToString() : "(missing block hash)");
-    file << strprintf("#   mined on %s\n", tip_height ? FormatISO8601DateTime(locked_chain->getBlockTime(*tip_height)) : "(missing block time)");
+    file << strprintf("# * Best block at time of backup was %i (%s),\n", pwallet->GetLastBlockHeight(), pwallet->GetLastBlockHash().ToString());
+    int64_t block_time = 0;
+    pwallet->chain().findBlock(pwallet->GetLastBlockHash(), nullptr, &block_time);
+    file << strprintf("#   mined on %s\n", block_time);
     file << "\n";
 
     // add the base58check encoded extended master if the wallet uses HD

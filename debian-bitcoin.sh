b11233287199a9cd722f136b220868f9a0f8afd2
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-06 06:52:52 +0000

    more debian vb config 	modified:   debian-bitcoin.sh

--- debian-bitcoin.sh
+++ debian-bitcoin.sh
@@ -1,2 +1,5 @@
-#!/usr/local/bin/env bash
+#!/usr/bin/env bash
 sudo apt-get -y update
+sudo apt-get -y  install git vim
+sudo su && echo "sudo mount -t vboxsf -o uid=1000,gid=1000 Share /home/user/vb-share" > /etc/rc.local
+

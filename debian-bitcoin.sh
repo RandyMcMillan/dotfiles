dc52d41676a0c12653ae1d5835397fb944b95d44
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-06 20:06:43 +0000

    begin conda config 	modified:   .bash_profile 	new file:   Miniconda3-latest-MacOSX-x86_64.sh 	new file:   condarc 	modified:   debian-bitcoin.sh

--- debian-bitcoin.sh
+++ debian-bitcoin.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
 sudo apt-get -y  install git vim
-sudo su && echo "sudo mount -t vboxsf -o uid=1000,gid=1000 Share /home/user/vb-share" > /etc/rc.local
+
+#we need to get some gpg keys onto the vm without a bunch of ssh piping
+mkdir ~/vb-share
+sudo su && echo "sudo mount -t vboxsf -o uid=1000,gid=1000 Share /home/git/vb-share" > /etc/rc.local
 

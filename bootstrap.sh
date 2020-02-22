fbce4a82aae0e39837b64b68fb9b831bfc9d9f7a
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-21 19:25:40 -0500

    add hosts config

--- bootstrap.sh
+++ bootstrap.sh
@@ -146,6 +146,22 @@ linkAndSource() {
 
 #linkAndSource
 
+configHOSTSfile() {
+
+#REF:https://github.com/StevenBlack/hosts.git
+
+    read -p "Config hosts file? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        git clone --depth 1 https://github.com/StevenBlack/hosts.git
+        cd hosts && python3 -m pip install --user -r requirements.txt
+        python3 testUpdateHostsFile.py
+        python3 updateHostsFile.py -a -r -e fakenews gambling porn social
+		fi
+
+}
+configHOSTSfile
+
 configGithub() {
 
     getpid;

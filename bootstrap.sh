f39683a2186bd980c6e9e1a6f9284c9a4b76acfd
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-23 00:42:09 -0500

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,6 @@
 taskCount=0
 tasksDone=0
 
-
 # usage getpid [varname]
 getpid(){
 
@@ -13,7 +12,6 @@ getpid(){
     #test "$1" && eval "$1=\$pid"
 
 }
-
 
 progressBarWidth=20
 
@@ -61,9 +59,8 @@ increment() {
 
 }
 
-
 installVim() {
-
+echo 'installVim'
     getpid;
 
     read -p "Install Vim? (y/n) " -n 1;
@@ -99,57 +96,65 @@ installVim() {
 
     done
     fi
+    echo
 
 }
-
 installVim
 
 linkAndSource() {
 
-    ## Collect task count
-    taskCount=12
-    tasksDone=0
+    read -p "link and source dotfiles? (y/n) " -n 1;
+    echo "";
 
-    getpid;
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo 'linkAndSource'
+        ## Collect task count
+        taskCount=12
+        tasksDone=0
 
-    while [ $tasksDone -le $taskCount ]; do
+        getpid;
 
-        ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-        increment
-        ln -sf ~/dotfiles/.bash_prompt  ~/.bash_prompt
-        increment
-        ln -sf ~/dotfiles/.functions    ~/.functions
-        increment
-        ln -sf ~/dotfiles/.gvimrc       ~/.gvimrc
-        increment
-        ln -sf ~/dotfiles/.macos        ~/.macos
-        increment
-        ln -sf ~/dotfiles/.osx          ~/.osx
-        increment
-        ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
-        increment
+        while [ $tasksDone -le $taskCount ]; do
 
-        source ~/.bash_profile;
-        increment
-        source ~/.bash_prompt;
-        increment
-        source ~/.functions
-        increment
-        source ~/.macos;
-        increment
-        source ~/.osx;
-        increment
+            ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+            increment
+            ln -sf ~/dotfiles/.bash_prompt  ~/.bash_prompt
+            increment
+            ln -sf ~/dotfiles/.functions    ~/.functions
+            increment
+            ln -sf ~/dotfiles/.gvimrc       ~/.gvimrc
+            increment
+            ln -sf ~/dotfiles/.osx          ~/.osx
+            increment
+            ln -sf ~/dotfiles/.macos        ~/.macos
+            increment
+            #REF:https://devhints.io/editorconfig
+            ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
+            increment
 
-    done
+            source ~/.bash_profile;
+            increment
+            source ~/.bash_prompt;
+            increment
+            source ~/.functions
+            increment
+            echo 'source ~/.osx'
+            source ~/.osx;
+            increment
+            #echo 'source ~/.macos'
+            #source ~/.macos;
+            #increment
+
+        done
+    fi
 
 }
-
-#linkAndSource
+linkAndSource
 
 configHOSTSfile() {
 
 #REF:https://github.com/StevenBlack/hosts.git
-
+echo 'configHOSTSfile'
     read -p "Config hosts file? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -157,7 +162,7 @@ configHOSTSfile() {
         cd hosts && python3 -m pip install --user -r requirements.txt
         python3 testUpdateHostsFile.py
         python3 updateHostsFile.py -a -r -e fakenews gambling porn social
-		fi
+    fi
 
 }
 configHOSTSfile
@@ -185,7 +190,6 @@ configGithub() {
     fi;
 
 }
-
 configGithub
 
 function doIt() {
@@ -197,8 +201,9 @@ function doIt() {
     getpid;
 
 while [ $tasksDone -le $taskCount ]; do
+echo 'rsync'
 
-    sudo rsync --exclude ".git/" \
+    rsync --exclude ".git/" \
           --exclude ".atom" \
           --exclude ".DS_Store" \
           --exclude ".vimrc" \
@@ -206,15 +211,9 @@ while [ $tasksDone -le $taskCount ]; do
           --exclude "README.md" \
           --exclude "LICENSE-MIT.txt" \
           --exclude "brew.sh" \
-          --exclude ".bash_profile" \
-          --exclude ".bash_prompt" \
           --exclude ".editorconfig" \
-          --exclude ".functions" \
-          --exclude ".gvimrc" \
-          --exclude ".macos" \
-          --exclude ".osx" \
-          --exclude ".editorconfig" \
-    -avh --no-perms . ~;
+          --exclude "hosts/" \
+          -avh --no-perms . ~;
     increment;
     done
 
@@ -224,7 +223,7 @@ while [ $tasksDone -le $taskCount ]; do
     getpid;
 
 while [ $tasksDone -le $taskCount ]; do
-
+echo 'line 218'
     if  csrutil status | grep 'disabled' &> /dev/null; then
         printf "System Integrity Protection status: \033[1;31mdisabled\033[0m\n";
         increment;
@@ -299,14 +298,18 @@ done
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 
+    echo 'getpid'
     getpid;
+    echo 'doIt'
     doIt;
 
 else
 
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
     if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo 'getpid'
         getpid;
+        echo 'doIt'
         doIt;
     fi;
 fi;

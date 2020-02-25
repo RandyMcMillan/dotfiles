6805bffeb368dfb36dded3b7915d4e73a3a5b299
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-24 23:57:31 -0500

    enable.sh and disable.sh

---
+++ enable.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo su -c "bash <(wget -qO- https://gist.githubusercontent.com/RandyMcMillan/634bc660e03151a037a97295b01a0369/raw/cc380356f86c37dfecb17f99b05067ee5c092247/enable.sh)" root
+

db9819db9043b5f3a9fdcfeeede4c635d964cdde
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-25 00:49:57 -0500

    modified:   installVim.sh

--- installVim.sh
+++ installVim.sh
@@ -8,36 +8,24 @@ echo 'installVim'
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-    ## Collect task count
-    taskCount=1
-    tasksDone=0
-
-    while [ $tasksDone -le $taskCount ]; do
 
         #sudo rm -rf ~/.vim_runtime
         if [ -d "$HOME/.vim_runtime/" ]; then
           cd ~/.vim_runtime
           git pull -f  origin master
-          increment
           sh ~/.vim_runtime/install_awesome_vimrc.sh
-          increment
           #we exclude from ~/ because we link to here
           ln -sf ~/dotfiles/.vimrc ~/.vim_runtime/my_configs.vim
-          increment
 
         else
 
           git clone --depth=1 https://github.com/randymcmillan/vimrc.git ~/.vim_runtime
-          increment
           sh ~/.vim_runtime/install_awesome_vimrc.sh
-          increment
           ln -sf ~/dotfiles/.vimrc ~/.vim_runtime/my_configs.vim
-          increment
         fi
 
-    done
     fi
     echo
 
 }
-#installVim
+installVim

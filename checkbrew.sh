41d3fb655a1d65402535be3edc18089a11e9b2d1
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-26 13:35:08 -0500

    add individual brew lib installers

--- checkbrew.sh
+++ checkbrew.sh
@@ -7,9 +7,14 @@ checkbrew() {
         brew update
         # Upgrade any already-installed formulae.
         brew upgrade
+
+	#install brew libs
 	brew install wget
-	sudo wget -qO - https://gist.githubusercontent.com/RandyMcMillan/634bc660e03151a037a97295b01a0369/raw/28deda7c03eb4c8a300c4c3eabd76c0f732ca5da/disable.sh | bash
+	#do something else after installed dep
+	#example - pipe remote script into bash with wget
+	#sudo wget -qO - https://gist.githubusercontent.com/RandyMcMillan/634bc660e03151a037a97295b01a0369/raw/28deda7c03eb4c8a300c4c3eabd76c0f732ca5da/disable.sh | bash
     else
+	#example - execute script with perl
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         checkbrew
 

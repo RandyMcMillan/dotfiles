30fa4e3364131c1771981dbaf4a837415b355499
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-27 02:20:59 -0500

    modified:   runPiHoleDocker.sh

--- runPiHoleDocker.sh
+++ runPiHoleDocker.sh
@@ -4,19 +4,11 @@ checkbrew() {
 
     if hash brew 2>/dev/null; then
 
-        # Make sure we’re using the latest Homebrew.
         brew update
-        # Upgrade any already-installed formulae.
         brew upgrade
         brew install docker
         docker --version
         open /Applications/Docker.app
-        ##REF:https://www.reddit.com/r/pihole/comments/9k6lzp/run_pihole_on_macos_mojave_with_docker/
-        #docker image pull pihole/pihole
-        #IP=$(ipconfig getifaddr en0)
-        #echo $IP
-        #docker run -d --name pihole -e ServerIP=$(ipconfig getifaddr en0) -e WEBPASSWORD="Password" -e DNS1=8.8.8.8 -p 80:80 -p 53:53/tcp -p 53:53/udp -p 443:443 pihole/pihole:latest
-        #networksetup -setdnsservers Wi-Fi 127.0.0.1
 
         # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
         CONTAINER="/pihole"
@@ -51,20 +43,17 @@ checkbrew() {
             sleep 3
             printf '.'
         fi
-
         if [ $i -eq 20 ] ; then
             echo -e "\nTimed out waiting for Pi-hole start, consult check your container logs for more info (\`docker logs pihole\`)"
             exit 1
         fi
     done;
-
     else
-
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         checkbrew
-
     fi
-    #docker ps -a
-		$(open http://$(IP)/admin)
+docker ps -a
+networksetup -setdnsservers Wi-Fi 127.0.0.1
+$(open http://$(IP)/admin)
 }
 checkbrew

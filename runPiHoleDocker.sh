c4de89e28d4782b7a0867feb68ec52c042f0e41e
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-27 14:02:54 -0500

    runPiHoleDocker.sh loop fixed

--- runPiHoleDocker.sh
+++ runPiHoleDocker.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 #run pi-hole as a docker service on macos
-checkbrew() {
+changePW() {
+echo "resetPW"
+docker exec -it pihole /bin/bash pihole -a -p
+}
+
+run() {
 
     if hash brew 2>/dev/null; then
 
         brew update
         brew upgrade
         brew install docker
-        docker --version
+        #docker --version
         open /Applications/Docker.app
 
         # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
@@ -20,7 +25,6 @@ checkbrew() {
             /usr/local/bin/docker rm   --force $CONTAINER
         fi
 
-        export IP=$(ipconfig getifaddr en0)
     docker run -d \
         --name pihole \
         -p 53:53/tcp -p 53:53/udp \
@@ -37,8 +41,8 @@ checkbrew() {
     for i in $(seq 1 20); do
         if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
             printf ' OK'
-            echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: http://${IP}/admin/"
-            exit 0
+            #exit 0
+            return 0
         else
             sleep 3
             printf '.'
@@ -47,13 +51,32 @@ checkbrew() {
             echo -e "\nTimed out waiting for Pi-hole start, consult check your container logs for more info (\`docker logs pihole\`)"
             exit 1
         fi
+
     done;
     else
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         checkbrew
     fi
-docker ps -a
-networksetup -setdnsservers Wi-Fi 127.0.0.1
-$(open http://$(IP)/admin)
 }
-checkbrew
+run
+
+#ADD your docker commands here to config the container
+
+changePW() {
+echo "resetPW"
+docker exec -it pihole /bin/bash pihole -a -p
+}
+changePW
+
+#host machine config #or nested VM's config
+configNetwork(){
+networksetup -setdnsservers Wi-Fi 127.0.0.1
+networksetup -setdnsservers Ethernet 127.0.0.1
+#SEARCHDOMAIN=$(networksetup -getsearchdomains en0) #use this to preserve existing search domains
+SEARCHDOMAIN=duckduckgo.com
+sudo networksetup -setsearchdomains Ethernet $SEARCHDOMAIN # additionalDomain
+sudo networksetup -setsearchdomains Wi-Fi $SEARCHDOMAIN # additionalDomain
+}
+configNetwork
+
+$(open http://$(ipconfig getifaddr en0)/admin)

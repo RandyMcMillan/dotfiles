aa1fd21e26215a7623dae3169bfd1b2485f0cd0b
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-04-04 15:36:40 -0400

    modified:   configHOSTSfile.sh

--- configHOSTSfile.sh
+++ configHOSTSfile.sh
@@ -2,15 +2,15 @@
 
 configHOSTSfile() {
 
-#REF:https://github.com/StevenBlack/hosts.git
+#REF:https://github.com/StevenBlack/hosts
 echo 'configHOSTSfile'
     read -p "Config hosts file? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        git clone --depth 1 https://github.com/randymcmillan/hosts.git
+        git clone --depth 1 https://github.com/StevenBlack/hosts.git
         cd hosts && python3 -m pip install --user -r requirements.txt
         python3 testUpdateHostsFile.py
-        python3 updateHostsFile.py -a -r -e fakenews gambling porn social
+        python3 updateHostsFile.py -a -r -e fakenews gambling porn
     fi
 
 }

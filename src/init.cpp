bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

refactor: Make CCheckQueue RAII-styled
Also this commits gets rid of scriptcheckqueue global variable.

src/init.cpp
@@ -217,7 +217,6 @@ void Shutdown(NodeContext& node)
     if (node.scheduler) node.scheduler->stop();
     threadGroup.interrupt_all();
     threadGroup.join_all();
-    StopScriptCheckWorkerThreads();
 
     // After the threads that potentially access these pointers have been stopped,
     // destruct and reset all to nullptr.
@@ -1290,7 +1289,6 @@ bool AppInitMain(NodeContext& node)
     LogPrintf("Script verification uses %d additional threads\n", script_threads);
     if (script_threads >= 1) {
         g_parallel_script_checks = true;
-        StartScriptCheckWorkerThreads(script_threads);
     }
 
     assert(!node.scheduler);
@@ -1538,7 +1536,7 @@ bool AppInitMain(NodeContext& node)
             const int64_t load_block_index_start_time = GetTimeMillis();
             try {
                 LOCK(cs_main);
-                g_chainman.InitializeChainstate();
+                g_chainman.InitializeChainstate(uint256(), script_threads);
                 UnloadBlockIndex();
 
                 // new CBlockTreeDB tries to delete the existing file, which

RandyMcMillan/dotfiles

src/init.cpp
@@ -218,7 +218,6 @@ void Shutdown(NodeContext& node)
     if (node.scheduler) node.scheduler->stop();
     threadGroup.interrupt_all();
     threadGroup.join_all();
-    StopScriptCheckWorkerThreads();
 
     // After the threads that potentially access these pointers have been stopped,
     // destruct and reset all to nullptr.
@@ -1308,7 +1307,6 @@ bool AppInitMain(const util::Ref& context, NodeContext& node)
     LogPrintf("Script verification uses %d additional threads\n", script_threads);
     if (script_threads >= 1) {
         g_parallel_script_checks = true;
-        StartScriptCheckWorkerThreads(script_threads);
     }
 
     assert(!node.scheduler);
@@ -1558,7 +1556,7 @@ bool AppInitMain(const util::Ref& context, NodeContext& node)
             const int64_t load_block_index_start_time = GetTimeMillis();
             try {
                 LOCK(cs_main);
-                chainman.InitializeChainstate();
+                chainman.InitializeChainstate(uint256(), script_threads);
                 UnloadBlockIndex();
 
                 // new CBlockTreeDB tries to delete the existing file, which
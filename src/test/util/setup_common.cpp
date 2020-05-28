2dbdb4355b873223388b3c2a737b1911bc179e43
Russell Yanofsky <russ@yanofsky.org>
2020-05-28 16:35:15 -0400

    test: Remove duplicate NodeContext hacks
    Qt tests currently are currently using two NodeContext structs at the same
    time, one in interfaces::NodeImpl::m_context, and the other in
    BasicTestingSetup::m_node, and the tests have hacks transferring state between
    them.
    
    Fix this by getting rid of the NodeImpl::m_context struct and making it a
    pointer. This way a common BitcoinApplication object can be used for all qt
    tests, but they can still have their own testing setups.
    
    Non-test code is changing but non-test behavior is still the same as before.
    
    Motivation for this PR is to be able to remove the
    "std::move(test.m_node.connman)" and mempool hacks for swapping individual
    NodeContext members in Qt tests, because followup PR #19099 adds yet another
    member (wallet_client) that needs to be swapped. After this change, the whole
    NodeContext struct can be swapped instead of individual members, so the
    workarounds are less fragile and invasive.

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -10,6 +10,7 @@
 #include <consensus/params.h>
 #include <consensus/validation.h>
 #include <crypto/sha256.h>
+#include <interfaces/chain.h>
 #include <init.h>
 #include <miner.h>
 #include <net.h>
@@ -32,6 +33,7 @@
 #include <util/vector.h>
 #include <validation.h>
 #include <validationinterface.h>
+#include <walletinitinterface.h>
 
 #include <functional>
 
@@ -104,6 +106,8 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName, const std::ve
     SetupNetworking();
     InitSignatureCache();
     InitScriptExecutionCache();
+    m_node.chain = interfaces::MakeChain(m_node);
+    g_wallet_init_interface.Construct(m_node);
     fCheckBlockIndex = true;
     static bool noui_connected = false;
     if (!noui_connected) {

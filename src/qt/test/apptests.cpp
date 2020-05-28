8569817a4341220d734d445b5846bb01b57fa303
Russell Yanofsky <russ@yanofsky.org>
2020-05-28 16:35:15 -0400

    test: Remove duplicate NodeContext hacks
    Qt tests currently are currently using two NodeContext structs at the same
    time, one in interfaces::NodeImpl::m_context, and the other in
    BasicTestingSetup::m_node, and the tests have hacks transferring state between
    them.
    
    Fix this by getting rid of the NodeImpl::m_context struct and making it a
    pointer. This way a common BitcoinApplication object can be used for all qt
    tests, but they can still have their own testing setups.
    
    Non-test code is changing but non-test behavior is still the same as before.
    
    Benefit of this PR is to being able to remove the existing
    "std::move(test.m_node.connman)" and mempool hacks.
    
    Motivation for this PR is to let qt wallet tests continue working when the
    ::vpwallets global variable is removed #19101 and not have to add even more
    complicated hacks exposing duplicate WalletContext and WalletClient instances
    in addition to the duplicate NodeContext instances and having to manipulate
    them in the test setup.

--- src/qt/test/apptests.cpp
+++ src/qt/test/apptests.cpp
@@ -6,6 +6,7 @@
 
 #include <chainparams.h>
 #include <key.h>
+#include <interfaces/node.h>
 #include <qt/bitcoin.h>
 #include <qt/bitcoingui.h>
 #include <qt/networkstyle.h>
@@ -63,6 +64,7 @@ void AppTests::appTests()
 #endif
 
     BasicTestingSetup test{CBaseChainParams::REGTEST}; // Create a temp data directory to backup the gui settings to
+    m_app.node().setContext(&test.m_node);
     ECC_Stop(); // Already started by the common test setup, so stop it to avoid interference
     LogInstance().DisconnectTestLogger();
 

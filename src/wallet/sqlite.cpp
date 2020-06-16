99b1cd44619b1c3f067ee3f6653186aaaf5bd491
Andrew Chow <achow101-github@achow101.com>
2020-06-16 15:38:12 -0400

    Implement SQLiteDatabase::MakeBatch

--- src/wallet/sqlite.cpp
+++ src/wallet/sqlite.cpp
@@ -254,7 +254,8 @@ void SQLiteDatabase::Close()
 
 std::unique_ptr<DatabaseBatch> SQLiteDatabase::MakeBatch(const char* mode, bool flush_on_close)
 {
-    return nullptr;
+    // We ignore flush_on_close because we don't do manual flushing for SQLite
+    return MakeUnique<SQLiteBatch>(*this, mode);
 }
 
 SQLiteBatch::SQLiteBatch(SQLiteDatabase& database, const char* mode)

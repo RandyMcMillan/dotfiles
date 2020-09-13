95b5dfa4da8c876c7c1e9e0bbc8ae9ccb97c9370
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-09-13 11:44:43 +0300

    ci: Use LOG=1 when building depends

--- ci/test/05_before_script.sh
+++ ci/test/05_before_script.sh
@@ -44,7 +44,7 @@ if [ -z "$NO_DEPENDS" ]; then
   else
     SHELL_OPTS="CONFIG_SHELL="
   fi
-  DOCKER_EXEC $SHELL_OPTS make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
+  DOCKER_EXEC $SHELL_OPTS make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS LOG=1
 fi
 if [ -n "$PREVIOUS_RELEASES_TO_DOWNLOAD" ]; then
   BEGIN_FOLD previous-versions

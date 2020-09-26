ryanofsky/bitcoin

Multiprocess bitcoin
This change only affects bitcoin-node, bitcoin-wallet, and bitcoin-gui
executables, which aren't built by default (they can be enabled with the
--enable-multiprocess autoconf option). Normal bitcoind and bitcoin-qt build
outputs are unaffected by this change.

After this change, bitcoin-gui will spawn a bitcoin-node process, and
bitcoin-node will spawn a bitcoin-wallet process. The processes communicate
across socket pairs so gui, node, and wallet functionality is segregated into
respective processes. The IPC calls can be seen running with -debug=ipc.  See
doc/multiprocess.md for details.

Work in progress, many improvements and cleanups are needed (see FIXMEs)

src/ipc/capnp/init-types.h
@@ -5,6 +5,22 @@
 #ifndef BITCOIN_IPC_CAPNP_INIT_TYPES_H
 #define BITCOIN_IPC_CAPNP_INIT_TYPES_H
 
+#include <ipc/capnp/chain-types.h>
 #include <ipc/capnp/echo.capnp.proxy-types.h>
+#include <ipc/capnp/node-types.h>
+
+namespace mp {
+//! Specialization of makeWalletClient needed because it takes a Chain& reference
+//! argument, not a unique_ptr<Chain> argument, so a manual cleanup
+//! callback is needed to clean up the ProxyServer<messages::Chain> proxy object.
+template <>
+struct ProxyServerMethodTraits<ipc::capnp::messages::Init::MakeWalletClientParams>
+{
+    using Context = ServerContext<ipc::capnp::messages::Init,
+                                  ipc::capnp::messages::Init::MakeWalletClientParams,
+                                  ipc::capnp::messages::Init::MakeWalletClientResults>;
+    static capnp::Void invoke(Context& context);
+};
+} // namespace mp
 
 #endif // BITCOIN_IPC_CAPNP_INIT_TYPES_H

RandyMcMillan/dotfiles

src/interfaces/capnp/init-types.h
@@ -5,6 +5,22 @@
 #ifndef BITCOIN_INTERFACES_CAPNP_INIT_TYPES_H
 #define BITCOIN_INTERFACES_CAPNP_INIT_TYPES_H
 
+#include <interfaces/capnp/chain-types.h>
 #include <interfaces/capnp/echo.capnp.proxy-types.h>
+#include <interfaces/capnp/node-types.h>
+
+namespace mp {
+//! Specialization of makeWalletClient needed because it takes a Chain& reference
+//! argument, not a unique_ptr<Chain> argument, so a manual addCloseHook()
+//! callback is needed to clean up the ProxyServer<messages::Chain> proxy object.
+template <>
+struct ProxyServerMethodTraits<interfaces::capnp::messages::Init::MakeWalletClientParams>
+{
+    using Context = ServerContext<interfaces::capnp::messages::Init,
+        interfaces::capnp::messages::Init::MakeWalletClientParams,
+        interfaces::capnp::messages::Init::MakeWalletClientResults>;
+    static std::unique_ptr<interfaces::WalletClient> invoke(Context& context);
+};
+} // namespace mp
 
 #endif // BITCOIN_INTERFACES_CAPNP_INIT_TYPES_H
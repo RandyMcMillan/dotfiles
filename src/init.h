ryanofsky/bitcoin

multiprocess: Add -ipcconnect and -ipcbind options
Add `-ipcbind` option to `bitcoin-node`, and an `-ipcconnect` option to
`bitcoin-wallet` to allow running a node that listens on an IPC socket and
accepts connections from wallet processes.

Example usage:

    src/bitcoin-node -regtest -debug -ipcbind=unix
    src/bitcoin-wallet -regtest -ipcconnect=unix info

`bitcoin-wallet` tool doesn't really do anything with its connection to the
node yet, but it could potentially run or serve RPCs that require being online.

src/init.h
@@ -66,7 +66,7 @@ bool AppInitMain(const util::Ref& context, NodeContext& node, interfaces::BlockA
 /**
  * Register all arguments with the ArgsManager
  */
-void SetupServerArgs(ArgsManager& argsman);
+void SetupServerArgs(ArgsManager& argsman, bool can_listen_ipc=false);
 
 /** Returns licensing information (for -version) */
 std::string LicenseInfo();

RandyMcMillan/dotfiles

src/init.h
@@ -66,7 +66,7 @@ bool AppInitMain(const util::Ref& context, NodeContext& node, interfaces::BlockA
 /**
  * Register all arguments with the ArgsManager
  */
-void SetupServerArgs(ArgsManager& argsman);
+void SetupServerArgs(ArgsManager& argsman, bool include_ipc=false);
 
 /** Returns licensing information (for -version) */
 std::string LicenseInfo();
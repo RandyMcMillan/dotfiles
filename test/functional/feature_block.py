ryanofsky/bitcoin

Increase feature_block.py and feature_taproot.py timeouts
Needed because BlockConnected notifications are a lot slower with the wallet
running in separate process.

test/functional/feature_block.py
@@ -90,6 +90,7 @@ class FullBlockTest(BitcoinTestFramework):
             '-acceptnonstdtxn=1',  # This is a consensus block test, we don't care about tx policy
             '-testactivationheight=bip34@2',
         ]]
+        self.rpc_timeout = 1920
 
     def run_test(self):
         node = self.nodes[0]  # convenience reference to the node
@@ -1290,7 +1291,7 @@ class FullBlockTest(BitcoinTestFramework):
         blocks2 = []
         for i in range(89, LARGE_REORG_SIZE + 89):
             blocks2.append(self.next_block("alt" + str(i)))
-        self.send_blocks(blocks2, False, force_send=False)
+        self.send_blocks(blocks2, False, force_send=False, timeout=1920)
 
         # extend alt chain to trigger re-org
         block = self.next_block("alt" + str(chain1_tip + 1))
@@ -1299,7 +1300,7 @@ class FullBlockTest(BitcoinTestFramework):
         # ... and re-org back to the first chain
         self.move_tip(chain1_tip)
         block = self.next_block(chain1_tip + 1)
-        self.send_blocks([block], False, force_send=True)
+        self.send_blocks([block], False, force_send=True, timeout=1920)
         block = self.next_block(chain1_tip + 2)
         self.send_blocks([block], True, timeout=2440)
 
@@ -1314,6 +1315,13 @@ class FullBlockTest(BitcoinTestFramework):
         b_cb34.hashMerkleRoot = b_cb34.calc_merkle_root()
         b_cb34.solve()
         self.send_blocks([b_cb34], success=False, reject_reason='bad-cb-height', reconnect=True)
+
+        # Flush the notification queue before shutting down, so the
+        # FlushBackgroundCallbacks call made during shutdown won't exceed the
+        # test framework's 60 second shutdown timeout on slow systems, due to
+        # all the BlockConnected notifications generated during the test.
+        self.log.info("Wait for BlockConnected notifications to be processed before shutdown")
+        self.nodes[0].syncwithvalidationinterfacequeue()
 
     # Helper methods
     ################

RandyMcMillan/dotfiles

test/functional/feature_block.py
@@ -83,6 +83,7 @@ class FullBlockTest(BitcoinTestFramework):
         self.num_nodes = 1
         self.setup_clean_chain = True
         self.extra_args = [['-acceptnonstdtxn=1']]  # This is a consensus block test, we don't care about tx policy
+        self.rpc_timeout = 1920
 
     def run_test(self):
         node = self.nodes[0]  # convenience reference to the node
@@ -1271,7 +1272,7 @@ class FullBlockTest(BitcoinTestFramework):
         blocks2 = []
         for i in range(89, LARGE_REORG_SIZE + 89):
             blocks2.append(self.next_block("alt" + str(i)))
-        self.send_blocks(blocks2, False, force_send=True)
+        self.send_blocks(blocks2, False, force_send=True, timeout=1920)
 
         # extend alt chain to trigger re-org
         block = self.next_block("alt" + str(chain1_tip + 1))
@@ -1280,7 +1281,7 @@ class FullBlockTest(BitcoinTestFramework):
         # ... and re-org back to the first chain
         self.move_tip(chain1_tip)
         block = self.next_block(chain1_tip + 1)
-        self.send_blocks([block], False, force_send=True)
+        self.send_blocks([block], False, force_send=True, timeout=1920)
         block = self.next_block(chain1_tip + 2)
         self.send_blocks([block], True, timeout=2440)
 
@@ -1295,6 +1296,13 @@ class FullBlockTest(BitcoinTestFramework):
         b_cb34.hashMerkleRoot = b_cb34.calc_merkle_root()
         b_cb34.solve()
         self.send_blocks([b_cb34], success=False, reject_reason='bad-cb-height', reconnect=True)
+
+        # Flush the notification queue before shutting down, so the
+        # FlushBackgroundCallbacks call made during shutdown won't exceed the
+        # test framework's 60 second shutdown timeout on slow systems, due to
+        # all the BlockConnected notifications generated during the test.
+        self.log.info("Wait for BlockConnected notifications to be processed before shutdown")
+        self.nodes[0].syncwithvalidationinterfacequeue()
 
     # Helper methods
     ################
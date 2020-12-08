b15c4f8bdc3a2fbd56a663f39cad6ef3b8c72154
Russell Yanofsky <russ@yanofsky.org>
2020-12-08 13:37:41 -0500

    Update libmultiprocess library
    Fix "Disable GCC suggest-override warnings for proxy clients" https://github.com/chaincodelabs/libmultiprocess/pull/40 is needed to prevent cirrus GCC failure https://cirrus-ci.com/task/6000489311502336?command=ci#L4294
    
    This also includes other recent changes
    
    https://github.com/chaincodelabs/libmultiprocess/pull/35 Fix README.md markdown
    https://github.com/chaincodelabs/libmultiprocess/pull/37 Add "make check" target to build and run tests
    https://github.com/chaincodelabs/libmultiprocess/pull/38 Add "extends" inherited method support

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=5741d750a04e644a03336090d8979c6d033e32c0
+$(package)_version=4dcd80741e867785100c761d1d8aea4dbdf89d15
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=ac848db49a6ed53e423c62d54bd87f1f08cbb0326254a8667e10bbfe5bf032a4
+$(package)_sha256_hash=9fe68761c491f80da509dd6f5d9703b2221e9d3d998b37eb680c62b82c0d0ff8
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds

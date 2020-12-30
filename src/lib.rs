eaec08d51cc60c4206f4ec3650cff592bbd03bde
Owez <root@ogriffiths.com>
2020-12-30 00:04:02 +0000

    Removed doc examples for private items

--- src/lib.rs
+++ src/lib.rs
@@ -284,21 +284,6 @@ impl<'a> Argument<'a> {
     ///
     /// # Example
     ///
-    /// Usage:
-    ///
-    /// ```rust
-    /// use std::io;
-    /// use climake::prelude::*;
-    ///
-    /// fn main() {
-    ///     let arg = Argument::new(
-    ///         "Verbose mode", vec!['v'], vec!["verbose"], Input::None
-    ///     );
-    ///
-    ///     arg.help_name_msg(&mut io::stdout()).unwrap();
-    /// }
-    /// ```
-    ///
     /// What this may look like:
     ///
     /// ```none
@@ -430,21 +415,6 @@ impl<'a> Subcommand<'a> {
     /// for simplicity, perf and extendability reasons.
     ///
     /// # Example
-    ///
-    /// Usage:
-    ///
-    /// ```rust
-    /// use std::io;
-    /// use climake::prelude::*;
-    ///
-    /// fn main() {
-    ///     let subcmd = Subcommand::new(
-    ///         "example", vec![], vec![], "A simple example subcommand"
-    ///     );
-    ///
-    ///     subcmd.help_name_msg(&mut io::stdout()).unwrap();
-    /// }
-    /// ```
     ///
     /// What this may look like:
     ///
@@ -641,21 +611,6 @@ impl<'a> CliMake<'a> {
     ///
     /// # Example
     ///
-    /// Usage:
-    ///
-    /// ```rust
-    /// use std::io;
-    /// use climake::prelude::*;
-    ///
-    /// fn main() {
-    ///     let cli = CliMake::new(
-    ///         "My app", vec![], vec![], "A simple application", "0.1.0"
-    ///     );
-    ///
-    ///     cli.header_msg(None, &mut io::stdout()).unwrap();
-    /// }
-    /// ```
-    ///
     /// What this may display:
     ///
     /// ```none
@@ -706,28 +661,6 @@ impl<'a> CliMake<'a> {
     /// - [Argument::help_name_msg]: Help generation for single [Argument]s
     ///
     /// # Example
-    ///
-    /// Usage:
-    ///
-    /// ```rust
-    /// use std::io;
-    /// use climake::prelude::*;
-    ///
-    /// fn main() {
-    ///     let verbose = Argument::new(
-    ///         "Toggles verbose mode",
-    ///         vec!['v'],
-    ///         vec!["verbose"],
-    ///         Input::None
-    ///     );
-    ///
-    ///     let cli = CliMake::new(
-    ///         "My app", vec![&verbose], vec![], "A simple application", "0.1.0"
-    ///     );
-    ///
-    ///     cli.help_msg(&mut io::stdout()).unwrap();
-    /// }
-    /// ```
     ///
     /// What this may look like:
     ///

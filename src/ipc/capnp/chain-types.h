ryanofsky/bitcoin

Add capnp wrapper for Wallet interface

src/ipc/capnp/chain-types.h
@@ -8,6 +8,7 @@
 #include <ipc/capnp/chain.capnp.proxy.h>
 #include <ipc/capnp/common.capnp.proxy-types.h>
 #include <ipc/capnp/handler.capnp.proxy-types.h>
+#include <ipc/capnp/wallet.capnp.proxy.h>
 #include <rpc/server.h>
 
 #include <any>
@@ -33,6 +34,10 @@ struct mp::ProxyServerMethodTraits<ipc::capnp::messages::ChainClient::StartParam
                                        ipc::capnp::messages::ChainClient::StartParams,
                                        ipc::capnp::messages::ChainClient::StartResults>;
     static void invoke(ChainContext& context);
+    using WalletContext = ServerContext<ipc::capnp::messages::WalletLoader,
+                                        ipc::capnp::messages::ChainClient::StartParams,
+                                        ipc::capnp::messages::ChainClient::StartResults>;
+    static void invoke(WalletContext& context);
 };
 
 namespace mp {

src/ipc/capnp/wallet.h
@@ -1,0 +1,41 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_IPC_CAPNP_WALLET_H
+#define BITCOIN_IPC_CAPNP_WALLET_H
+
+#include <interfaces/wallet.h>
+#include <ipc/capnp/chain.capnp.proxy.h>
+#include <ipc/capnp/wallet.capnp.h>
+#include <mp/proxy.h>
+
+#include <future>
+#include <memory>
+
+class CScheduler;
+namespace interfaces {
+class WalletLoader;
+} // namespace interfaces
+namespace ipc {
+namespace capnp {
+namespace messages {
+struct WalletLoader;
+} // namespace messages
+} // namespace capnp
+} // namespace ipc
+
+//! Specialization of WalletLoader proxy server needed hold a CSCheduler instance.
+template <>
+struct mp::ProxyServerCustom<ipc::capnp::messages::WalletLoader, interfaces::WalletLoader>
+    : public mp::ProxyServerBase<ipc::capnp::messages::WalletLoader, interfaces::WalletLoader>
+{
+public:
+    using ProxyServerBase::ProxyServerBase;
+    void invokeDestroy();
+
+    std::unique_ptr<CScheduler> m_scheduler;
+    std::future<void> m_result;
+};
+
+#endif // BITCOIN_IPC_CAPNP_WALLET_H

RandyMcMillan/dotfiles

src/ipc/capnp/chain-types.h
@@ -8,6 +8,7 @@
 #include <ipc/capnp/chain.capnp.proxy.h>
 #include <ipc/capnp/common.capnp.proxy-types.h>
 #include <ipc/capnp/handler.capnp.proxy-types.h>
+#include <ipc/capnp/wallet.capnp.proxy.h>
 #include <rpc/server.h>
 
 #include <any>
@@ -33,6 +34,10 @@ struct mp::ProxyServerMethodTraits<ipc::capnp::messages::ChainClient::StartParam
                                        ipc::capnp::messages::ChainClient::StartParams,
                                        ipc::capnp::messages::ChainClient::StartResults>;
     static void invoke(ChainContext& context);
+    using WalletContext = ServerContext<ipc::capnp::messages::WalletClient,
+                                        ipc::capnp::messages::ChainClient::StartParams,
+                                        ipc::capnp::messages::ChainClient::StartResults>;
+    static void invoke(WalletContext& context);
 };
 
 namespace mp {

src/ipc/capnp/wallet.h
@@ -1,0 +1,41 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_IPC_CAPNP_WALLET_H
+#define BITCOIN_IPC_CAPNP_WALLET_H
+
+#include <interfaces/wallet.h>
+#include <ipc/capnp/chain.capnp.proxy.h>
+#include <ipc/capnp/wallet.capnp.h>
+#include <mp/proxy.h>
+
+#include <future>
+#include <memory>
+
+class CScheduler;
+namespace interfaces {
+class WalletClient;
+} // namespace interfaces
+namespace ipc {
+namespace capnp {
+namespace messages {
+struct WalletClient;
+} // namespace messages
+} // namespace capnp
+} // namespace ipc
+
+//! Specialization of WalletClient proxy server needed hold a CSCheduler instance.
+template <>
+struct mp::ProxyServerCustom<ipc::capnp::messages::WalletClient, interfaces::WalletClient>
+    : public mp::ProxyServerBase<ipc::capnp::messages::WalletClient, interfaces::WalletClient>
+{
+public:
+    using ProxyServerBase::ProxyServerBase;
+    void invokeDestroy();
+
+    std::unique_ptr<CScheduler> m_scheduler;
+    std::future<void> m_result;
+};
+
+#endif // BITCOIN_IPC_CAPNP_WALLET_H
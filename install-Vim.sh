17f3e76a1a58a2c73feda2bb20126f450a072edf
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-05-07 14:50:01 -0400

    rebase:autotools-example

--- install-Vim.sh
+++ install-Vim.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
-
-if hash git 2>/dev/null; then
+if [ hash git 2>/dev/nulli ]; then
     git config --global core.editor vim
     git config --global pull.rebase true
 else
-    if hash brew 2>/dev/null; then
+    if [ hash brew 2>/dev/null ]; then
         brew install git
     fi
-    if hash apt 2>/dev/null; then
+    if [ hash apt 2>/dev/null ]; then
         apt install git
     fi
-    if hash apk 2>/dev/null; then
+    if [ hash apk 2>/dev/null ]; then
         apk add git
     fi
 fi
 
+VIM=$(find /usr/local/Cellar/macvim -name vim)
+export VIM
+echo   $VIM
 install-vim() {
 #WE install this regaurdless of OSTYPE
-read -t 1 -p "Install Vim? (y/n) " -n 1;
-echo "";
+VIMRC_REPO="https://github.com/randymcmillan/vimrc.git"
+export VIMRC_REPO
+echo $VIMRC_REPO
+VIMRC_DESTINATION="$HOME/.vim_runtime"
+export VIMRC_DESTINATION
+echo $VIMRC_DESTINATION
+read -t 2 -p "Install .vim_runtime ? (y/n) " -n 1;
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    #sudo rm -rf ~/.vim_runtime
-    if [ -d "$HOME/.vim_runtime/" ]; then
+    if [ -d "$VIMRC_DESTINATION" ]; then
       cd ~/.vim_runtime
       git pull -f origin master
       sh ~/.vim_runtime/install_awesome_vimrc.sh
@@ -33,39 +39,45 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
       ln -sf ~/dotfiles/.vimrc ~/.vim_runtime/my_configs.vim
     fi
 fi
-echo
+}
+MACVIM=$(find /usr/local/Cellar/macvim -name MacVim.app)
+export MACVIM
+echo   $MACVIM
+MVIM=$(find /usr/local/Cellar/macvim -name mvim)
+export MVIM
+echo   $MVIM
+install-macvim(){
 if [[ "$OSTYPE" == "darwin"* ]]; then
     if hash brew 2>/dev/null; then
         if ! hash mvim 2>/dev/null; then
-            read -t 1 -p "Install MacVim? (y/n) " -n 1;
+            read -t 3 -p "Install MacVim? (y/n) " -n 1;
             echo "";
             if [[ $REPLY =~ ^[Yy]$ ]]; then
-                if  ! hash mvim 2>/dev/null; then
+                if [ ! hash mvim 2>/dev/null ]; then
                     brew install -f macvim
                     brew link --overwrite macvim
-                elif
-                    hash mvim 2>/dev/null; then
+                else
                     echo MacVim already installed.
                     echo
+                    brew upgrade macvim
                 fi
+            fi
+            if ! hash tccutil 2>/dev/null; then
+                brew install tccutil
+            fi
+            if ! hash dockutil 2>/dev/null; then
+                curl -k -o /usr/local/bin/dockutil https://raw.githubusercontent.com/kcrawford/dockutil/master/scripts/dockutil
+                chmod a+x /usr/local/bin/dockutil
+                MACVIM=$(find /usr/local/Cellar/macvim -name MacVim.app)
+                echo $MACVIM
+                export MACVIM
+                dockutil --add $MACVIM --replacing 'MacVim'
             fi
         fi
     else
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
-    if ! hash tccutil 2>/dev/null; then
-        brew install tccutil
-        if ! hash dockutil 2>/dev/null; then
-            curl -k -o /usr/local/bin/dockutil https://raw.githubusercontent.com/kcrawford/dockutil/master/scripts/dockutil
-            chmod a+x /usr/local/bin/dockutil
-        fi
-    else
-
-        MACVIM=$(find /usr/local/Cellar/macvim -name MacVim.app)
-        export MACVIM
-        dockutil --add $MACVIM --replacing 'MacVim'
-
-    fi
 fi
 }
 install-vim
+install-macvim
--- install-valgrind-macos.sh
+++ install-valgrind-macos.sh
@@ -1,24 +1,88 @@
 #!/usr/bin/env bash
+#ENV VARS
+OS=$(uname)
+OS_VERSION=$(uname -r)
+UNAME_M=$(uname -m)
+ARCH=$(uname -m)
+export OS
+export OS_VERSION
+export UNAME_M
+export ARCH
+
+report() {
+echo OS:
+echo "$OS" | awk '{print tolower($0)}'
+echo OS_VERSION:
+echo "$OS_VERSION" | awk '{print tolower($0)}'
+echo UNAME_M:
+echo "$UNAME_M" | awk '{print tolower($0)}'
+echo ARCH:
+echo "$ARCH" | awk '{print tolower($0)}'
+echo OSTYPE:
+echo "$OSTYPE" | awk '{print tolower($0)}'
+}
 
 checkbrew() {
 
     if hash brew 2>/dev/null; then
-    if ! hash wget 2>/dev/null; then
-        brew install wget
-    fi
-    if ! hash curl 2>/dev/null; then
-        brew install curl
-    fi
+        brew tap LouisBrunner/valgrind
+        brew install --HEAD LouisBrunner/valgrind/valgrind
+        brew upgrade --fetch-HEAD LouisBrunner/valgrind/valgrind
+        echo
     else
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         checkbrew
     fi
 }
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo
+checkraspi(){
+
+    echo 'Checking Raspi'
+    if [ -e /etc/rpi-issue ]; then
+    echo "- Original Installation"
+    cat /etc/rpi-issue
+    fi
+    if [ -e /usr/bin/lsb_release ]; then
+    echo "- Current OS"
+    lsb_release -irdc
+    fi
+    echo "- Kernel"
+    uname -r
+    echo "- Model"
+    cat /proc/device-tree/model && echo
+    echo "- hostname"
+    hostname
+    echo "- Firmware"
+    /opt/vc/bin/vcgencmd version
+}
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+    #CHECK APT
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        if hash apt 2>/dev/null; then
+            apt install valgrind
+            report
+            echo 'Using apt...'
+        fi
+    fi
+    if [[ "$OSTYPE" == "linux-musl" ]]; then
+        if hash apk 2>/dev/null; then
+            apk add awk
+            apk add valgrind
+            report
+            echo 'Using apk...'
+        fi
+    fi
+    if [[ "$OSTYPE" == "linux-arm"* ]]; then
+        checkraspi
+        if hash apt 2>/dev/null; then
+            apt install awk
+            report
+            apt install valgrind
+            echo 'Using apt...'
+        fi
+    fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     checkbrew
-    brew install --HEAD https://raw.githubusercontent.com/sowson/valgrind/master/valgrind.rb
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo TODO add support for $OSTYPE
 elif [[ "$OSTYPE" == "msys" ]]; then

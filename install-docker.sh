364f4c08d1208e4d2a5c8f04fac133c693a3fe9a
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-05-18 14:20:14 -0400

    git commit --allow-empty

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #ENV VARS
+
 OS=$(uname)
 OS_VERSION=$(uname -r)
 UNAME_M=$(uname -m)
@@ -11,21 +12,21 @@ export ARCH
 
 report() {
 echo OS:
-echo "$OS" | gawk '{print tolower($0)}'
+echo "$OS"         | $SUDO awk '{print tolower($0)}'
 echo OS_VERSION:
-echo "$OS_VERSION" | gawk '{print tolower($0)}'
+echo "$OS_VERSION" | $SUDO awk '{print tolower($0)}'
 echo UNAME_M:
-echo "$UNAME_M" | gawk '{print tolower($0)}'
+echo "$UNAME_M"    | $SUDO awk '{print tolower($0)}'
 echo ARCH:
-echo "$ARCH" | gawk '{print tolower($0)}'
+echo "$ARCH"       | $SUDO awk '{print tolower($0)}'
 echo OSTYPE:
-echo "$OSTYPE" | gawk '{print tolower($0)}'
+echo "$OSTYPE"     | $SUDO awk '{print tolower($0)}'
 }
 
 checkbrew() {
 
     if hash brew 2>/dev/null; then
-        brew install gawk git docker
+        brew install awk git docker docker-compose
         echo
     else
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -54,17 +55,22 @@ checkraspi(){
 }
 
 if [[ "$OSTYPE" == "linux"* ]]; then
+SUDO=''
+if (($EUID != 0)); then
+    SUDO='sudo'
+    export SUDO
+fi
     #CHECK APT
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         if hash apt 2>/dev/null; then
-            apt install gawk
+            $SUDO apt install gawk docker.io docker-compose
             report
             echo 'Using apt...'
         fi
     fi
     if [[ "$OSTYPE" == "linux-musl" ]]; then
         if hash apk 2>/dev/null; then
-            apk add gawk
+            $SUDO apk add gawk docker.io docker-compose
             report
             echo 'Using apk...'
         fi
@@ -72,11 +78,15 @@ if [[ "$OSTYPE" == "linux"* ]]; then
     if [[ "$OSTYPE" == "linux-arm"* ]]; then
         checkraspi
         if hash apt 2>/dev/null; then
-            apt install gawk
+            $SUDO apt install gawk docker.io docker-compose
             report
             echo 'Using apt...'
         fi
     fi
+    echo 'https://docs.docker.com/config/daemon/systemd'
+    echo 'running $SUDO systemctl start docker'
+    $SUDO systemctl start docker
+    ##$SUDO apt install $1
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     checkbrew
 elif [[ "$OSTYPE" == "cygwin" ]]; then

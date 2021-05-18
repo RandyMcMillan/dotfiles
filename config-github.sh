db6bf72783efadf13e8d240781b5ee9e1c1cad44
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-05-18 19:27:20 -0400

    add git config --global receive.denyCurrentBranch false

--- config-github.sh
+++ config-github.sh
@@ -4,6 +4,7 @@ git config --global commit.gpgsign true
 git config --global core.ignorecase false
 git config --global advice.addIgnoredFile false
 git config --global pull.rebase true
+git config --global receive.denyCurrentBranch ignore
 config-github() {
 
     read -p "Config github? (y/n) " -n 1;

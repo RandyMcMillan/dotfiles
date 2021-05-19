e2f976969770aaecc986f3530b669ac223d6f021
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-05-19 01:06:09 -0400

    new file:   install-awk.sh

---
+++ install-awk.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+#ENV VARS
+OS=$(uname)
+OS_VERSION=$(uname -r)
+UNAME_M=$(uname -m)
+ARCH=$(uname -m)
+export OS
+export OS_VERSION
+export UNAME_M
+export ARCH
+
+if [[ "$OSTYPE" == "Darwin"* ]]; then
+    AWK='awk'
+else
+    AWK='gawk'
+fi
+export AWK
+
+report() {
+echo OS:
+echo "$OS" | $AWK '{print tolower($0)}'
+echo OS_VERSION:
+echo "$OS_VERSION" | $AWK '{print tolower($0)}'
+echo UNAME_M:
+echo "$UNAME_M" | $AWK '{print tolower($0)}'
+echo ARCH:
+echo "$ARCH" | $AWK '{print tolower($0)}'
+echo OSTYPE:
+echo "$OSTYPE" | $AWK '{print tolower($0)}'
+}
+
+install-awk() {
+
+    if hash brew 2>/dev/null; then
+        brew install $AWK
+        echo
+    else
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        install-awk
+    fi
+}
+checkraspi(){
+
+    echo 'Checking Raspi'
+    if [ -e /etc/rpi-issue ]; then
+    echo "- Original Installation"
+    cat /etc/rpi-issue
+    fi
+    if [ -e /usr/bin/lsb_release ]; then
+    echo "- Current OS"
+    lsb_release -irdc
+    fi
+    echo "- Kernel"
+    uname -r
+    echo "- Model"
+    cat /proc/device-tree/model && echo
+    echo "- hostname"
+    hostname
+    echo "- Firmware"
+    /opt/vc/bin/vcgencmd version
+}
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+    #CHECK APT
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        if hash apt 2>/dev/null; then
+            apt install $AWK
+            report
+            echo 'Using apt...'
+        fi
+    fi
+    if [[ "$OSTYPE" == "linux-musl" ]]; then
+        if hash apk 2>/dev/null; then
+            apk add $AWK
+            report
+            echo 'Using apk...'
+        fi
+    fi
+    if [[ "$OSTYPE" == "linux-arm"* ]]; then
+        checkraspi
+        if hash apt 2>/dev/null; then
+            apt install $AWK
+            report
+            echo 'Using apt...'
+        fi
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    checkbrew
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo TODO add support for $OSTYPE
+elif [[ "$OSTYPE" == "msys" ]]; then
+    echo TODO add support for $OSTYPE
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo TODO add support for $OSTYPE
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo TODO add support for $OSTYPE
+else
+    echo TODO add support for $OSTYPE
+fi
+

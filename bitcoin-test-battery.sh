8695a9492ce0bff00c8d5eaa86c33b9a00f947dd
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-09-01 20:47:24 -0400

    more bitcoin-test-battery.sh

--- bitcoin-test-battery.sh
+++ bitcoin-test-battery.sh
@@ -6,36 +6,47 @@ export TIME
 RC=v22.0rc3
 export RC
 
-BITCOIN_CONF1="proxy=127.0.0.1:9050 #If you use Windows, this could possibly be 127.0.0.1:9150 in some cases. \n
-listen=1 \n
-bind=127.0.0.1 \n
-onlynet=onion \n
- \n
-connect=ygwcypmb2qiotrp3.onion:8333 \n
-connect=stcwaszw352kkjis.onion:8333 \n
-connect=nrrmkgmulpgsbwlt.onion \n
-connect=orrxr4kfntzawea3.onion:8333 \n
-connect=q2fhnnyt5b2ayvce.onion \n
-connect=rk4vbyca7xnn3top.onion \n
-connect=7ndbwnmgbyupv47f.onion \n
-connect=bitcoinzi27kiwf6.onion \n
-connect=w4ja2rt6wvqn6rfw.onion:8333 \n
-connect=6maigxjvcet4pite.onion:8333 \n
-connect=cehok4dxukhnoopi.onion:8333 \n
-connect=r6apa5ssujxbwd34.onion:8333 \n
-connect=ggvnc3v5pmrlsupw.onion:8333 \n
-connect=er4mwkhxzxgavrvo.onion:8333 \n
-connect=35yncj7et6k3koqy.onion \n
-connect=nesxfmano25clfvn.onion:8333 \n
-connect=5d5vtnm6xlsqzq7p.onion \n
-connect=34jtv2p5pw4e5bp3.onion \n
-connect=z5nt64xh4d3vnll2.onion \n
-connect=lncmdma3namzrbnx.onion:8333 \n
-connect=lncmdmx7ezlplcck.onion:8333 \n
-connect=lncmdmgoddecttey.onion:8444 \n
+BITCOIN_CONF1="proxy=127.0.0.1:9050 #If you use Windows, this could possibly be 127.0.0.1:9150 in some cases.\r
+listen=1\r
+bind=127.0.0.1\r
+onlynet=onion\r
+\r
+connect=ygwcypmb2qiotrp3.onion:8333\r
+connect=stcwaszw352kkjis.onion:8333\r
+connect=nrrmkgmulpgsbwlt.onion\r
+connect=orrxr4kfntzawea3.onion:8333\r
+connect=q2fhnnyt5b2ayvce.onion\r
+connect=rk4vbyca7xnn3top.onion\r
+connect=7ndbwnmgbyupv47f.onion\r
+connect=bitcoinzi27kiwf6.onion\r
+connect=w4ja2rt6wvqn6rfw.onion:8333\r
+connect=6maigxjvcet4pite.onion:8333\r
+connect=cehok4dxukhnoopi.onion:8333\r
+connect=r6apa5ssujxbwd34.onion:8333\r
+connect=ggvnc3v5pmrlsupw.onion:8333\r
+connect=er4mwkhxzxgavrvo.onion:8333\r
+connect=35yncj7et6k3koqy.onion\r
+connect=nesxfmano25clfvn.onion:8333\r
+connect=5d5vtnm6xlsqzq7p.onion\r
+connect=34jtv2p5pw4e5bp3.onion\r
+connect=z5nt64xh4d3vnll2.onion\r
+connect=lncmdma3namzrbnx.onion:8333\r
+connect=lncmdmx7ezlplcck.onion:8333\r
+connect=lncmdmgoddecttey.onion:8444\r
 "
 
+make-data-dir(){
+
+    mkdir -p /tmp/$TIME
+    export DATA_DIR=/tmp/$TIME
+    mkdir -p $DATA_DIR
+    echo -e $BITCOIN_CONF1 > $DATA_DIR/bitcoin.conf
+
+}
+
 doIt(){
+
+make-data-dir
 
 
     [[ $(find ~/$BITCOIN_TEST_BATTERY -type d 2>/dev/null) ]] && \
@@ -44,10 +55,6 @@ doIt(){
     pushd ~/$BITCOIN_TEST_BATTERY #&& make clean
     git checkout $RC
     pushd ~/$BITCOIN_TEST_BATTERY && ./autogen.sh && ./configure --with-gui=yes --with-sqlite=yes --without-bdb && make -j $(nproc --all)
-    mkdir -p /tmp/$TIME
-    export DATA_DIR=/tmp/$TIME
-    mkdir -p $DATA_DIR
-    echo -e $BITCOIN_CONF1 > $DATA_DIR/bitcoin.conf
     export BINARY_PATH=$(pwd)/src
     export QT_PATH=$(pwd)/src/qt
     export BINARY_PATH=$(pwd)/bin

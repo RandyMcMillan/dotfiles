05adb55e89caa937c2f4987918fa07125d0b48ab
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-09-23 16:07:32 -0400

    ...

--- template.sh
+++ template.sh
@@ -1,4 +1,82 @@
 #!/usr/bin/env bash
+
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/bin/bash-oo-framework/lib/oo-bootstrap.sh"
+
+# load the type system
+import util/log util/exception util/tryCatch util/namedParameters
+import util/variable
+
+# using colors:
+echo "$(UI.Color.Blue)I'm blue...$(UI.Color.Default)"
+
+# enable basic logging for this file by declaring a namespace
+namespace myApp
+# make the Log method direct everything in the namespace 'myApp' to the log handler called DEBUG
+Log::AddOutput myApp DEBUG
+
+# now we can write with the DEBUG output set
+Log "Play me some Jazz, will ya? $(UI.Powerline.Saxophone)"
+
+# redirect error messages to STDERR
+Log::AddOutput error STDERR
+subject=error Log "Something bad happened."
+
+# reset outputs
+Log::ResetAllOutputsAndFilters
+
+# You may also hardcode the use for the StdErr output directly:
+Console::WriteStdErr "This will be printed to STDERR, no matter what."
+
+try {
+    # something...
+    rm -rf ~/test
+    touch ~/test
+    cp ~/test ~/test2
+    # something more...
+} catch {
+    echo "The hard disk is not connected properly!"
+    echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
+    echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+
+    ## printing a caught exception couldn't be simpler, as it's stored in "${__EXCEPTION__[@]}"
+    Exception::PrintException "${__EXCEPTION__[@]}"
+}
+
+#array someArray=( 'one' 'two' )
+## the above is an equivalent of: declare -a someArray=( 'one' 'two' )
+## except this one creates a $var:someArray method handler
+#
+#passingArraysInput() {
+#  [array] passedInArray
+#
+#  # chained usage, see below for more details:
+#  $var:passedInArray : \
+#    { map 'echo "${index} - $(var: item)"' } \
+#    { forEach 'var: item toUpper' }
+#
+#  $var:passedInArray push 'will work only for references'
+#}
+#
+#echo 'passing by $var:'
+#
+### 2 ways of passing a copy of an array (passing by it's definition)
+#passingArraysInput "$(@get someArray)"
+#passingArraysInput $var:someArray
+#
+### no changes yet
+#$var:someArray toJSON
+#
+#echo
+#echo 'passing by $ref:'
+#
+### in bash >=4.3, which supports references, you may pass by reference
+### this way any changes done to the variable within the function will affect the variable itself
+#passingArraysInput $ref:someArray
+#
+### should show changes
+#$var:someArray toJSON
+
+
 #ENV VARS
 OS=$(uname)
 OS_VERSION=$(uname -r)
@@ -28,6 +106,7 @@ checkbrew() {
         if !hash git 2>/dev/null; then
             brew install git
         fi
+        brew install bash
     else
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         checkbrew

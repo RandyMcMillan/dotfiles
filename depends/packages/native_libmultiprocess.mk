679bfa517701b558c01de59b1737ad61368b55ed
Russell Yanofsky <russ@yanofsky.org>
2021-10-25 14:30:47 -0400

    Update libmultiprocess library
    Fix std::move error https://github.com/chaincodelabs/libmultiprocess/pull/58

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=d576d975debdc9090bd2582f83f49c76c0061698
+$(package)_version=7d10f3b1e39caa04b3fcddebf720fd3a2b54c21d
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=9f8b055c8bba755dc32fe799b67c20b91e7b13e67cadafbc54c0f1def057a370
+$(package)_sha256_hash=1e669deea827756c396e9740cac6581c3c8758e206320d359c520fc76ff31f53
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds

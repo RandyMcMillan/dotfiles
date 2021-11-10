86e87ce7a55e3699305c5e8f26f5fe34f18b1b80
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-11-10 14:19:08 -0500

    config-github.sh: better config

--- config-github.sh
+++ config-github.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-./config-git.sh
 config-github() {
+
+    DATE=$(date +%s)
 
     read -p "Config github? (y/n) " -n 1;
     echo "";
@@ -16,39 +17,80 @@ config-github() {
     #REF:https://help.github.com/en/github/authenticating-to-github/checking-for-existing-gpg-keys
     gpg --list-secret-keys --keyid-format LONG
     read -p 'ENTER your GPG Signing Key: ' GPG_SIGNING_KEY
-    #git config --global user.signingkey $GPG_SIGNING_KEY
-    git config --global user.signingkey 97966C06BB06757B
+    git config --global user.signingkey $GPG_SIGNING_KEY
+    #git config --global user.signingkey 97966C06BB06757B
     echo && echo
     echo Your GPG Siging Key has been added...
     echo && echo
     fi;
 
+    mkdir -p ~/.ssh
     chmod 700 ~/.ssh
+    touch     ~/.ssh/authorized_keys
     chmod 600 ~/.ssh/authorized_keys
+    touch     ~/.ssh/known_hosts
     chmod 644 ~/.ssh/known_hosts
+    touch     ~/.ssh/config
     chmod 644 ~/.ssh/config
 
-    DATE=$(date +%s)
+    mv ~/.ssh/config ~/.ssh/config.bak.$DATE
+    echo "Host *" > ~/.ssh/config
+    echo "  AddKeysToAgent yes" >> ~/.ssh/config
 
-    ssh-keygen -t rsa -b 4096 -N '' -C $GITHUB_USER_EMAIL -f ~/.ssh/$DATE.github_rsa
-    ssh-keygen -t rsa -b 4096 -N '' -C $GITHUB_USER_EMAIL -f ~/.ssh/$DATE.docker_rsa
+    GITHUB_RSA=~/.ssh/$DATE.github_rsa
+    DOCKER_RSA=~/.ssh/$DATE.docker_rsa
+    DO_RSA=~/.ssh/$DATE.do_rsa
+    ssh-keygen -t rsa -b 4096 -N '' -C $GITHUB_USER_EMAIL -f $GITHUB_RSA
+    echo "  IdentityFile $GITHUB_RSA" >> ~/.ssh/config
+    ssh-keygen -t rsa -b 4096 -N '' -C $GITHUB_USER_EMAIL -f $DOCKER_RSA
+    echo "  IdentityFile $DOCKER_RSA" >> ~/.ssh/config
+    ssh-keygen -t rsa -b 4096 -N '' -C $GITHUB_USER_EMAIL -f $DO_RSA
+    echo "  IdentityFile $DO_RSA" >> ~/.ssh/config
 
-    sudo chmod 600 ~/.ssh/$DATE.github_rsa
-    sudo chmod 644 ~/.ssh/$DATE.github_rsa.pub
-    sudo chmod 600 ~/.ssh/$DATE.docker_rsa
-    sudo chmod 644 ~/.ssh/$DATE.docker_rsa.pub
+    sudo chmod 600 $GITHUB_RSA
+    sudo chmod 644 $GITHUB_RSA.pub
+    sudo chmod 600 $DOCKER_RSA
+    sudo chmod 644 $DOCKER_RSA.pub
+    sudo chmod 600 $DO_RSA
+    sudo chmod 644 $DO_RSA.pub
 
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/github_rsa
     ssh-add ~/.ssh/docker_rsa
-    ssh-add ~/.ssh/$DATE.github_rsa
-    ssh-add ~/.ssh/$DATE.docker_rsa
-		ssh-add ~/.ssh/*_rsa
+    ssh-add ~/.ssh/do_rsa
+    ssh-add $GITHUB_RSA
+    ssh-add $DOCKER_RSA
+    ssh-add $DO_RSA
 
-    echo "Copy this pub key to your github profile..."
-    cat ~/.ssh/$DATE.github_rsa.pub
-    echo "a docker key pair"
-    cat ~/.ssh/$DATE.docker_rsa.pub
-		echo https://gorails.com/setup/osx/10.14-mojave
+    echo
+    cat ~/.ssh/config
+    echo
+    ls -lT $GITHUB_RSA.pub
+    echo
+    echo "Copy these pub keys to your github profile..."
+    echo
+    echo
+    ls -lT $GITHUB_RSA.pub
+    echo  "$GITHUB_RSA.pub"
+    echo
+    cat $GITHUB_RSA.pub
+    echo
+    echo
+    ls -lT $DOCKER_RSA.pub
+    echo  "$DOCKER_RSA.pub"
+    echo
+    cat $DOCKER_RSA.pub
+    echo
+    echo
+    ls -lT $DO_RSA.pub
+    echo  "$DO_RSA.pub"
+    echo
+    cat $DO_RSA.pub
+    echo
+    echo
 }
+eval "$(ssh-agent -s)"
 config-github
+ssh-add ~/.ssh/*.github_rsa
+./config-git.sh
+

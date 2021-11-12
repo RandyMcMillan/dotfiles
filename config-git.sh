687ea31a0b7bbb6948f0779e6987a85b3b2a3839
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-11-12 16:25:58 -0500

    config-git.sh

--- config-git.sh
+++ config-git.sh
@@ -4,5 +4,6 @@ git config --global commit.gpgsign true
 git config --global core.ignorecase false
 git config --global advice.addIgnoredFile false
 git config --global pull.rebase true
+git config pull.rebase true
 git config --global alias.prune-branches-force '!git remote prune origin && git branch -vv | grep '"'"': gone]'"'"' | awk '"'"'{print $1}'"'"' | xargs git branch -D'
 git config --global alias.prune-branches '!git remote prune origin && git branch -vv | grep '"'"': gone]'"'"' | awk '"'"'{print $1}'"'"' | xargs git branch -d'

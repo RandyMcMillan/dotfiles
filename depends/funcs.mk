81c9fd38772d881a0013afadd1007db7269f3d2f
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-01-02 13:12:00 +0200

    build: Make built dependency packages reproducible

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -228,7 +228,9 @@ $($(1)_postprocessed): | $($(1)_staged)
 	touch $$@
 $($(1)_cached): | $($(1)_dependencies) $($(1)_postprocessed)
 	echo Caching $(1)...
-	cd $$($(1)_staging_dir)/$(host_prefix); find . | sort | tar --no-recursion -czf $$($(1)_staging_dir)/$$(@F) -T -
+	cd $$($(1)_staging_dir)/$(host_prefix); \
+	  find . ! -name '.stamp_postprocessed' -print0 | TZ=UTC xargs -0 touch -m -t 200001011200; \
+	  find . ! -name '.stamp_postprocessed' -print0 | LC_ALL=C sort -z | tar --create --numeric-owner --no-recursion --null -T - | gzip -9n > $$($(1)_staging_dir)/$$(@F)
 	mkdir -p $$(@D)
 	rm -rf $$(@D) && mkdir -p $$(@D)
 	mv $$($(1)_staging_dir)/$$(@F) $$(@)

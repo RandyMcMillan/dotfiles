b8725f4e391f96a6175577c535e375d9adf3d55f
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-01-06 19:40:29 -0500

    path scaling better with these settings

--- src/qt/mempool_tab/mempoolconstants.h
+++ src/qt/mempool_tab/mempoolconstants.h
@@ -17,9 +17,8 @@ static const int     GRAPH_PADDING_TOP           = 50;
 static const int     GRAPH_PADDING_TOP_LABEL     = 0;
 static const int     GRAPH_PADDING_BOTTOM        = 50; //qreal bottom
 //scalar
-static const double  GRAPH_MAXHEIGHT_G_SCALAR    = 0.9;
-
-static const double  GRAPH_PATH_SCALAR           = 0.5;
+static const double  GRAPH_MAXHEIGHT_G_SCALAR    = 0.7;//0.7 with 0.5 scales well
+static const double  GRAPH_PATH_SCALAR           = 0.5;//0.7 with 0.5 scales well
 
 static const double  GRAPH_HORZ_LINE_SCALAR      = 1.0;
 

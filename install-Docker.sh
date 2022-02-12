979a502cee6bc187dbf1ce62a52ad4f253cd295f
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-12 17:18:21 -0500

    rebase:autotools-example:update from randymcmillan on 1644704301

--- install-Docker.sh
+++ install-Docker.sh
@@ -1,34 +1,10 @@
 #!/usr/bin/env bash
-#ENV VARS
-OS=$(uname)
-OS_VERSION=$(uname -r)
-UNAME_M=$(uname -m)
-ARCH=$(uname -m)
-export OS
-export OS_VERSION
-export UNAME_M
-export ARCH
-report() {
-echo OS:
-echo "$OS" | awk '{print tolower($0)}'
-echo OS_VERSION:
-echo "$OS_VERSION" | awk '{print tolower($0)}'
-echo UNAME_M:
-echo "$UNAME_M" | awk '{print tolower($0)}'
-echo ARCH:
-echo "$ARCH" | awk '{print tolower($0)}'
-echo OSTYPE:
-echo "$OSTYPE" | awk '{print tolower($0)}'
-}
-checkbrew() {
+source ./checkbrew.sh
+source ./checkraspi.sh
+checkbrew
+checkraspi
+install-docker() {
     if hash brew 2>/dev/null; then
-        export HOMEBREW_NO_INSTALL_CLEANUP=false
-        if ! hash $AWK 2>/dev/null; then
-            brew install $AWK
-        fi
-        if ! hash git 2>/dev/null; then
-            brew install git
-        fi
         #brew install docker                      || brew install --cask docker             || brew upgrade --cask docker
         #brew install docker-compose              || brew install docker-compose            || brew upgrade docker-compose
         #brew install docker-edge                 || brew install --cask docker-edge || brew upgrade --cask docker-edge
@@ -61,112 +37,44 @@ checkbrew() {
         checkbrew
     fi
 }
-checkraspi(){
-    echo 'Checking Raspi'
-    if [ -e /etc/rpi-issue ]; then
-    echo "- Original Installation"
-    cat /etc/rpi-issue
-    fi
-    if [ -e /usr/bin/lsb_release ]; then
-    echo "- Current OS"
-    lsb_release -irdc
-    fi
-    echo "- Kernel"
-    uname -r
-    echo "- Model"
-    cat /proc/device-tree/model && echo
-    echo "- hostname"
-    hostname
-    echo "- Firmware"
-    /opt/vc/bin/vcgencmd version
-}
-
-if [[ "$OSTYPE" == "linux"* ]]; then
-    #CHECK APT
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        PACKAGE_MANAGER=apt
-        export PACKAGE_MANAGER
-        INSTALL=install
-        export INSTALL
-        AWK=gawk
-        export AWK
-        if hash apt 2>/dev/null; then
-            $PACKAGE_MANAGER $INSTALL $AWK
-            #report
-        fi
-        checkbrew
-    fi
-    if [[ "$OSTYPE" == "linux-musl" ]]; then
-        PACKAGE_MANAGER=apk
-        export PACKAGE_MANAGER
-        INSTALL=install
-        export INSTALL
-        AWK=gawk
-        export AWK
-        if hash apk 2>/dev/null; then
-            $PACKAGE_MANAGER $INSTALL $AWK
-            #report
-        fi
-        checkbrew
-    fi
-    if [[ "$OSTYPE" == "linux-arm"* ]]; then
-        PACKAGE_MANAGER=apt
-        export PACKAGE_MANAGER
-        INSTALL=install
-        export INSTALL
-        AWK=gawk
-        echo $AWK
-        export AWK
-        checkraspi
-        if hash apt 2>/dev/null; then
-            $PACKAGE_MANAGER $INSTALL $AWK
-            #report
-            # Install some required packages first
-            sudo apt update
-            sudo apt install -y \
-            apt-transport-https \
-            ca-certificates \
-            curl \
-            gnupg2 \
-            software-properties-common
-
-            # Get the Docker signing key for packages
-            curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-
-            # Add the Docker official repos
-            echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-            $(lsb_release -cs) stable" | \
-            sudo tee /etc/apt/sources.list.d/docker.list
-
-            # Install Docker
-            sudo apt update
-            sudo apt install -y --no-install-recommends \
-                docker-ce \
-                cgroupfs-mount
-            sudo systemctl enable docker
-            sudo systemctl start docker
-        fi
-        checkbrew
-    fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+function install-docker-arm(){
+if [[ "$OSTYPE" == "linux-arm"* ]]; then
+    PACKAGE_MANAGER=apt
+    export PACKAGE_MANAGER
+    INSTALL=install
+    export INSTALL
+    AWK=gawk
+    echo $AWK
+    export AWK
+    checkraspi
+    if hash apt 2>/dev/null; then
+        $PACKAGE_MANAGER $INSTALL $AWK
         #report
-        PACKAGE_MANAGER=$(which brew)
-        export PACKAGE_MANAGER
-        echo $PACKAGE_MANAGER
-        INSTALL=install
-        export INSTALL
-        AWK=awk
-        export AWK
-        checkbrew
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-    echo TODO add support for $OSTYPE
-elif [[ "$OSTYPE" == "msys" ]]; then
-    echo TODO add support for $OSTYPE
-elif [[ "$OSTYPE" == "win32" ]]; then
-    echo TODO add support for $OSTYPE
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    echo TODO add support for $OSTYPE
-else
-    echo TODO add support for $OSTYPE
-fi
+        # Install some required packages first
+        sudo apt update
+        sudo apt install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg2 \
+        software-properties-common
 
+        # Get the Docker signing key for packages
+        curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+        # Add the Docker official repos
+        echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+        $(lsb_release -cs) stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list
+
+        # Install Docker
+        sudo apt update
+        sudo apt install -y --no-install-recommends \
+            docker-ce \
+            cgroupfs-mount
+        sudo systemctl enable docker
+        sudo systemctl start docker
+    fi
+    checkbrew
+fi
+}

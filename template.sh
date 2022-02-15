c538ec87927d07d8fc44b590fb2df6bed4cb936c
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-14 20:33:49 -0500

    add not root guard to template.sh

--- template.sh
+++ template.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 HOMEBREW_NO_INSTALL_CLEANUP=fale
 export  HOMEBREW_NO_INSTALL_CLEANUP
-
 function checkbrew() {
+if [ "$EUID" -ne "0" ]; then
     if hash brew 2>/dev/null; then
         if ! hash $AWK 2>/dev/null; then
             brew install $AWK
@@ -14,4 +14,7 @@ function checkbrew() {
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         checkbrew
     fi
+else
+    echo "home brew prevents being installed from root!!!\nTry\nmake adduser-git"
+fi
 }

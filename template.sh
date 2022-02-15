781de7fa0306c9eba39855c0f49f33412d935991
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-14 20:38:24 -0500

    brew: detect awk - unlink if exists

--- template.sh
+++ template.sh
@@ -4,8 +4,13 @@ export  HOMEBREW_NO_INSTALL_CLEANUP
 function checkbrew() {
 if [ "$EUID" -ne "0" ]; then
     if hash brew 2>/dev/null; then
+        export AWK=gawk
         if ! hash $AWK 2>/dev/null; then
+        if hash awk 2>/dev/null; then
+            brew unlink awk
+        fi
             brew install $AWK
+            brew link $AWK
         fi
         if ! hash git 2>/dev/null; then
             brew install git

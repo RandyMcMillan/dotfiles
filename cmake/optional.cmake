01d9fd3f2e3914339f47316e38d04ccce1fa6d71
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-05-04 15:46:55 +0200

    cmake: Add `ccache` support

---
+++ cmake/optional.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) 2022 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+# Optional features and packages.
+
+set(USE_CCACHE "auto" CACHE STRING "Use ccache for building ([auto], yes, no). \"auto\" means \"yes\" if ccache is found")
+
+set(OPTION_VALUES auto yes no)
+foreach(option USE_CCACHE)
+  if(NOT ${option} IN_LIST OPTION_VALUES)
+    message(FATAL_ERROR "${option} value is \"${${option}}\", but must be one of \"auto\", \"yes\" or \"no\".")
+  endif()
+endforeach()
+
+if(NOT USE_CCACHE STREQUAL no)
+  find_program(CCACHE ccache)
+  if(CCACHE)
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+  elseif(USE_CCACHE STREQUAL yes)
+    message(FATAL_ERROR "ccache requested, but not found.")
+  endif()
+endif()

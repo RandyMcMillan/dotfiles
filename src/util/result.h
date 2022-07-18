4953196af11d3c186023064dcdab825697f3fa44
Ryan Ofsky <ryan@ofsky.org>
2022-07-18 15:48:04 -0400

    refactor: Add util::Result failure values, multiple error and warning messages
    Add util::Result support for returning more error information.
    
    This change adds two major features to the result class:
    
    - For better error handling, adds the ability to return a value on failure, not
      just a value on success. This is a key missing feature that makes the result
      class not useful for functions like LoadChainstate() which produce different
      errors that need to be handled differently.
    
    - For better error reporting, adds the ability to return warning messages and
      multiple errors, not just a single error string. This provides a way for
      functions to report errors and warnings in a standard way, and simplifies
      interfaces.
    
    This change also makes some more minor improvements:
    
    - Smaller type size. util::Result<int> is 16 bytes, and util::Result<void> is 8
      bytes. Previously util::Result<int> was 72 bytes and util::Result<void> was
      unimplemented.
    
    - Broader type compatibility. Supports Result<void> return values, so it is
      possible for functions that don't return values to still report errors.
      Supports noncopyable and nonmovable success and error types.

--- src/util/result.h
+++ src/util/result.h
@@ -8,16 +8,135 @@
 #include <attributes.h>
 #include <util/translation.h>
 
+#include <memory>
+#include <optional>
+#include <tuple>
 #include <variant>
+#include <vector>
 
 namespace util {
+namespace detail {
+//! Empty string list
+const std::vector<bilingual_str> EMPTY_LIST{};
 
+//! Helper to move elements from one container to another.
+template <typename T>
+void MoveElements(T& src, T& dest)
+{
+    dest.insert(dest.end(), std::make_move_iterator(src.begin()), std::make_move_iterator(src.end()));
+    src.clear();
+}
+
+//! Error information only allocated if there are errors or warnings.
+template <typename F>
+struct ErrorInfo {
+    std::optional<std::conditional_t<std::is_same<F, void>::value, std::monostate, F>> failure;
+    std::vector<bilingual_str> errors;
+    std::vector<bilingual_str> warnings;
+};
+
+//! Result base class which is inherited by Result<T, F>.
+template <typename T, typename F>
+class ResultBase;
+
+//! Result base specialization for empty (T=void) value type. Holds error
+//! information and provides accessor methods.
+template <typename F>
+class ResultBase<void, F>
+{
+protected:
+    std::unique_ptr<ErrorInfo<F>> m_info;
+
+    template <typename... Args>
+    void InitFailure(Args&&... args)
+    {
+        if (!m_info) m_info = std::make_unique<ErrorInfo<F>>();
+        m_info->failure.emplace(std::forward<Args>(args)...);
+    }
+    void InitValue() {}
+    template <typename O>
+    void MoveValue(O& other) {}
+    void DestroyValue() {}
+
+public:
+    void AddError(bilingual_str error)
+    {
+        if (error.empty()) return;
+        if (!m_info) m_info = std::make_unique<ErrorInfo<F>>();
+        m_info->errors.emplace_back(std::move(error));
+    }
+
+    void AddWarning(bilingual_str warning)
+    {
+        if (warning.empty()) return;
+        if (!m_info) m_info = std::make_unique<ErrorInfo<F>>();
+        m_info->warnings.emplace_back(std::move(warning));
+    }
+
+    //! Success check.
+    operator bool() const { return !m_info || !m_info->failure; }
+
+    //! Error retrieval.
+    template <typename _F = F>
+    std::enable_if_t<!std::is_same<_F, void>::value, const _F&> GetFailure() const { assert(!*this); return *m_info->failure; }
+    const std::vector<bilingual_str>& GetErrors() const { return m_info ? m_info->errors : EMPTY_LIST; }
+    const std::vector<bilingual_str>& GetWarnings() const { return m_info ? m_info->warnings : EMPTY_LIST; }
+};
+
+//! Result base class for T value type. Holds value and provides accessor methods.
+template <typename T, typename F>
+class ResultBase : public ResultBase<void, F>
+{
+protected:
+    //! Result success value. Uses anonymous union so success value is never
+    //! constructed in failure case.
+    union { T m_value; };
+
+    ResultBase() {}
+    ~ResultBase() {}
+
+    template <typename... Args>
+    void InitValue(Args&&... args) { new (&m_value) T{std::forward<Args>(args)...}; }
+    template <typename O>
+    void MoveValue(O& other) { new (&m_value) T{std::move(other.m_value)}; }
+    void DestroyValue() { m_value.~T(); }
+
+    template <typename FT, typename FF>
+    friend class ResultBase;
+
+public:
+    //! std::optional methods, so functions returning optional<T> can change to
+    //! return Result<T> with minimal changes to existing code, and vice versa.
+    bool has_value() const { return *this; }
+    const T& value() const LIFETIMEBOUND { assert(*this); return m_value; }
+    T& value() LIFETIMEBOUND { assert(*this); return m_value; }
+    template <class U>
+    T value_or(U&& default_value) const&
+    {
+        return has_value() ? value() : std::forward<U>(default_value);
+    }
+    template <class U>
+    T value_or(U&& default_value) &&
+    {
+        return has_value() ? std::move(value()) : std::forward<U>(default_value);
+    }
+    const T* operator->() const LIFETIMEBOUND { return &value(); }
+    const T& operator*() const LIFETIMEBOUND { return value(); }
+    T* operator->() LIFETIMEBOUND { return &value(); }
+    T& operator*() LIFETIMEBOUND { return value(); }
+};
+} // namespace detail
+
+//! Wrapper types to pass error and warning strings to Result constructors.
 struct Error {
     bilingual_str message;
 };
+struct Warning {
+    bilingual_str message;
+};
 
-//! The util::Result class provides a standard way for functions to return
-//! either error messages or result values.
+//! The util::Result class provides a standard way for functions to return error
+//! and warning strings in addition to optional result values.
 //!
 //! It is intended for high-level functions that need to report error strings to
 //! end users. Lower-level functions that don't need this error-reporting and
@@ -31,54 +150,86 @@ struct Error {
 //! `std::optional<T>` can be updated to return `util::Result<T>` and return
 //! error strings usually just replacing `return std::nullopt;` with `return
 //! util::Error{error_string};`.
-template <class T>
-class Result
+template <typename T, typename F = void>
+class Result : public detail::ResultBase<T, F>
 {
-private:
-    std::variant<bilingual_str, T> m_variant;
+protected:
+    template <typename Fn, typename... Args>
+    void Construct(const Fn& fn, Args&&... args)
+    {
+        fn(std::forward<Args>(args)...);
+    }
 
-    template <typename FT>
-    friend bilingual_str ErrorString(const Result<FT>& result);
+    template <typename Fn, typename... Args>
+    void Construct(const Fn& fn, Error error, Args&&... args)
+    {
+        this->AddError(std::move(error.message));
+        Construct([this](auto&&... x) { this->InitFailure(std::forward<decltype(x)>(x)...); }, std::forward<Args>(args)...);
+    }
+
+    template <typename Fn, typename... Args>
+    void Construct(const Fn& fn, Warning warning, Args&&... args)
+    {
+        this->AddWarning(std::move(warning.message));
+        Construct(fn, std::forward<Args>(args)...);
+    }
+
+    template <typename Fn, typename OT, typename OF, typename... Args>
+    void Construct(const Fn& fn, Result<OT, OF>&& other, Args&&... args)
+    {
+        *this << std::move(other);
+        Construct(fn, std::forward<Args>(args)...);
+    }
+
+    template <typename OT, typename OF>
+    void MoveConstruct(Result<OT, OF>& other)
+    {
+        *this << std::move(other);
+        if (other) this->MoveValue(other); else if (other.m_info) this->m_info->failure = std::move(other.m_info->failure);
+    }
+
+    template <typename FT, typename FF>
+    friend class Result;
 
 public:
-    Result(T obj) : m_variant{std::in_place_index_t<1>{}, std::move(obj)} {}
-    Result(Error error) : m_variant{std::in_place_index_t<0>{}, std::move(error.message)} {}
+    //! Constructors, destructor, and assignment operator.
+    template <typename... Args>
+    Result(Args&&... args)
+    {
+        Construct([this](auto&&... x) { this->InitValue(std::forward<decltype(x)>(x)...); }, std::forward<Args>(args)...);
+    }
+    template <typename OT, typename OF>
+    Result(Result<OT, OF>&& other) { MoveConstruct(other); }
+    Result& operator=(Result&& other)
+    {
+        if (*this) this->DestroyValue(); else this->m_info->failure.reset();
+        MoveConstruct(other);
+        return *this;
+    }
+    ~Result() { if (*this) this->DestroyValue(); }
 
-    //! std::optional methods, so functions returning optional<T> can change to
-    //! return Result<T> with minimal changes to existing code, and vice versa.
-    bool has_value() const noexcept { return m_variant.index() == 1; }
-    const T& value() const LIFETIMEBOUND
+    //! Operator moving warning and error messages from one result to another.
+    //! Only moves message strings, does not change success or failure values of
+    //! either Result object.
+    template <typename OT, typename OF>
+    Result<OT, OF>&& operator<<(Result<OT, OF>&& other)
     {
-        assert(has_value());
-        return std::get<1>(m_variant);
+        if (other.m_info && !this->m_info) {
+            this->m_info.reset(new detail::ErrorInfo<F>{.errors = std::move(other.m_info->errors),
+                                                        .warnings = std::move(other.m_info->warnings)});
+        } else if (other.m_info && this->m_info) {
+            detail::MoveElements(other.m_info->errors, this->m_info->errors);
+            detail::MoveElements(other.m_info->warnings, this->m_info->warnings);
+        }
+        return std::move(other);
     }
-    T& value() LIFETIMEBOUND
-    {
-        assert(has_value());
-        return std::get<1>(m_variant);
-    }
-    template <class U>
-    T value_or(U&& default_value) const&
-    {
-        return has_value() ? value() : std::forward<U>(default_value);
-    }
-    template <class U>
-    T value_or(U&& default_value) &&
-    {
-        return has_value() ? std::move(value()) : std::forward<U>(default_value);
-    }
-    explicit operator bool() const noexcept { return has_value(); }
-    const T* operator->() const LIFETIMEBOUND { return &value(); }
-    const T& operator*() const LIFETIMEBOUND { return value(); }
-    T* operator->() LIFETIMEBOUND { return &value(); }
-    T& operator*() LIFETIMEBOUND { return value(); }
 };
 
-template <typename T>
-bilingual_str ErrorString(const Result<T>& result)
-{
-    return result ? bilingual_str{} : std::get<0>(result.m_variant);
-}
+//! Helper methods to format error strings.
+bilingual_str ErrorString(const std::vector<bilingual_str>& errors);
+bilingual_str ErrorString(const std::vector<bilingual_str>& errors, const std::vector<bilingual_str>& warnings);
+template <typename T, typename F>
+bilingual_str ErrorString(const Result<T, F>& result) { return ErrorString(result.GetErrors(), result.GetWarnings()); }
 } // namespace util
 
 #endif // BITCOIN_UTIL_RESULT_H

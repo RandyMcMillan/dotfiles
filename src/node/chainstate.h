65481de0646f21349f24327410e4d7eb5189e5b3
Ryan Ofsky <ryan@ofsky.org>
2022-07-21 00:46:20 -0400

    refactor: Use util::Result class in LoadChainstate and VerifyLoadedChainstate

--- src/node/chainstate.h
+++ src/node/chainstate.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_NODE_CHAINSTATE_H
 #define BITCOIN_NODE_CHAINSTATE_H
 
+#include <util/result.h>
 #include <util/translation.h>
 #include <validation.h>
 
@@ -31,14 +32,11 @@ struct ChainstateLoadOptions {
     std::function<void()> coins_error_cb;
 };
 
-//! Chainstate load status. Simple applications can just check for the success
-//! case, and treat other cases as errors. More complex applications may want to
-//! try reindexing in the generic failure case, and pass an interrupt callback
-//! and exit cleanly in the interrupted case.
-enum class ChainstateLoadStatus { SUCCESS, FAILURE, FAILURE_INCOMPATIBLE_DB, INTERRUPTED };
-
-//! Chainstate load status code and optional error string.
-using ChainstateLoadResult = std::tuple<ChainstateLoadStatus, bilingual_str>;
+//! Chainstate load errors. Simple applications can just treat all errors as
+//! failures. More complex applications may want to try reindexing in the
+//! generic error case, and pass an interrupt callback and exit cleanly in the
+//! interrupted case.
+enum class ChainstateLoadError { FAILURE, FAILURE_INCOMPATIBLE_DB, INTERRUPTED };
 
 /** This sequence can have 4 types of outcomes:
  *
@@ -53,9 +51,9 @@ using ChainstateLoadResult = std::tuple<ChainstateLoadStatus, bilingual_str>;
  *
  *  LoadChainstate returns a (status code, error string) tuple.
  */
-ChainstateLoadResult LoadChainstate(ChainstateManager& chainman, const CacheSizes& cache_sizes,
-                                    const ChainstateLoadOptions& options);
-ChainstateLoadResult VerifyLoadedChainstate(ChainstateManager& chainman, const ChainstateLoadOptions& options);
+util::Result<void, ChainstateLoadError> LoadChainstate(ChainstateManager& chainman, const CacheSizes& cache_sizes,
+                                                       const ChainstateLoadOptions& options);
+util::Result<void, ChainstateLoadError> VerifyLoadedChainstate(ChainstateManager& chainman, const ChainstateLoadOptions& options);
 } // namespace node
 
 #endif // BITCOIN_NODE_CHAINSTATE_H

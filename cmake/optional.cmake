bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

cmake: Add `libzmq` optional package support

cmake/optional.cmake
@@ -25,8 +25,10 @@ option(ENABLE_NATPMP_DEFAULT "If NAT-PMP is enabled, turn it on at startup" OFF)
 set(WITH_MINIUPNPC "auto" CACHE STRING "Enable UPNP ([auto], yes, no). \"auto\" means \"yes\" if libminiupnpc is found")
 option(ENABLE_UPNP_DEFAULT "If UPNP is enabled, turn it on at startup" OFF)
 
+set(WITH_ZMQ "auto" CACHE STRING "Enable ZMQ notifications ([auto], yes, no). \"auto\" means \"yes\" if libzmq is found")
+
 set(OPTION_VALUES auto yes no)
-foreach(option USE_CCACHE WITH_SQLITE WITH_BDB WITH_NATPMP WITH_MINIUPNPC)
+foreach(option USE_CCACHE WITH_SQLITE WITH_BDB WITH_NATPMP WITH_MINIUPNPC WITH_ZMQ)
   if(NOT ${option} IN_LIST OPTION_VALUES)
     message(FATAL_ERROR "${option} value is \"${${option}}\", but must be one of \"auto\", \"yes\" or \"no\".")
   endif()
@@ -106,5 +108,30 @@ if(NOT WITH_MINIUPNPC STREQUAL "no")
                       "To skip libminiupnpc check, use \"-DWITH_MINIUPNPC=no\".\n")
     endif()
     set(WITH_MINIUPNPC no)
+  endif()
+endif()
+
+if(NOT WITH_ZMQ STREQUAL "no")
+  if(MSVC)
+    find_package(ZeroMQ CONFIG)
+  else()
+    pkg_check_modules(libzmq libzmq>=4 IMPORTED_TARGET GLOBAL)
+    if(libzmq_FOUND)
+      set_property(TARGET PkgConfig::libzmq APPEND PROPERTY
+        INTERFACE_COMPILE_DEFINITIONS $<$<PLATFORM_ID:Windows>:ZMQ_STATIC>
+      )
+      add_library(libzmq ALIAS PkgConfig::libzmq)
+    endif()
+  endif()
+  if(TARGET libzmq)
+    set(WITH_ZMQ yes)
+  else()
+    if(WITH_ZMQ STREQUAL "yes")
+      message(FATAL_ERROR "libzmq requested, but not found.")
+    else()
+      message(WARNING "libzmq not found, disabling.\n"
+                      "To skip libzmq check, use \"-DWITH_ZMQ=no\".\n")
+    endif()
+    set(WITH_ZMQ no)
   endif()
 endif()

RandyMcMillan/dotfiles

cmake/optional.cmake
@@ -100,3 +100,28 @@ if(WITH_MINIUPNPC)
     set(WITH_MINIUPNPC OFF)
   endif()
 endif()
+
+if(WITH_ZMQ)
+  if(MSVC)
+    find_package(ZeroMQ CONFIG)
+  else()
+    pkg_check_modules(libzmq libzmq>=4 IMPORTED_TARGET GLOBAL)
+    if(libzmq_FOUND)
+      set_property(TARGET PkgConfig::libzmq APPEND PROPERTY
+        INTERFACE_COMPILE_DEFINITIONS $<$<PLATFORM_ID:Windows>:ZMQ_STATIC>
+      )
+      add_library(libzmq ALIAS PkgConfig::libzmq)
+    endif()
+  endif()
+  if(TARGET libzmq)
+    set(WITH_ZMQ ON)
+  else()
+    if(WITH_ZMQ STREQUAL ON)
+      message(FATAL_ERROR "libzmq requested, but not found.")
+    else()
+      message(WARNING "libzmq not found, disabling.\n"
+                      "To skip libzmq check, use \"-DWITH_ZMQ=OFF\".\n")
+    endif()
+    set(WITH_ZMQ OFF)
+  endif()
+endif()
bb68f340afc68654621cc3c5edc8234f8b21e6a1
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-11-28 17:10:03 +0000

    Introduce platform-agnostic `ALWAYS_INLINE` macro

--- src/attributes.h
+++ src/attributes.h
@@ -16,4 +16,12 @@
 #  define LIFETIMEBOUND
 #endif
 
+#if defined(__GNUC__)
+#  define ALWAYS_INLINE inline __attribute__((always_inline))
+#elif defined(_MSC_VER)
+#  define ALWAYS_INLINE __forceinline
+#else
+#  define ALWAYS_INLINE inline
+#endif
+
 #endif // BITCOIN_ATTRIBUTES_H

b4c8a4242fc41789be1dad15ac4f6efc51fab7e8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-19 13:31:55 -0500

    rebase:autotools-example:make: add autoconf sequence

--- funcs.mk
+++ funcs.mk
@@ -5,3 +5,8 @@ funcs-1:
 .PHONY: install-dotfiles-on-remote
 install-dotfiles-on-remote:
 	./install-dotfiles-on-remote.sh
+.PHONY: submodule submodules
+submodule: submodules
+submodules:
+	git submodule update --init --recursive
+	git submodule foreach 'git fetch origin; git checkout $$(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'

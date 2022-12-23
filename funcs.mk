5fc4ea605a19c3fa8e0dbadd2a6a93128289289a
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-23 04:20:48 -0500

    rebase:autotools-example:.gitmodules: make submodules/deinit
    modified:   .gitmodules
    modified:   funcs.mk

--- funcs.mk
+++ funcs.mk
@@ -7,10 +7,13 @@ install-dotfiles-on-remote:
 	./install-dotfiles-on-remote.sh
 .PHONY: submodule submodules
 submodule: submodules
+##	:	submodules		git submodule --init --recursive
 submodules:
 	git submodule update --init --recursive
 	git submodule foreach 'git fetch origin; git checkout $$(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
-
+##	:	submodules-deinit	git submodule deinit --all -f
+submodules-deinit:
+	@git submodule deinit --all -f
 
 # vim: set noexpandtab:
 # vim: set setfiletype make

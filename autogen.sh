ebd883545e75b56e8675dc5716ece367fe748f48
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-23 19:08:43 -0500

    autogen.sh: autoreconf -vfi --install --force --warnings=all

--- autogen.sh
+++ autogen.sh
@@ -13,4 +13,4 @@ if [ -z "${LIBTOOLIZE}" ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
 fi
 command -v autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf --install --force --warnings=all
+autoreconf -vfi --install --force --warnings=all

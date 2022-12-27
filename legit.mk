66577e7025bf26c16dbfe3ffce9fa800fe6df7f0
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-27 16:49:34 -0500

    make: help legit nostril 02-002a657a

--- legit.mk
+++ legit.mk
@@ -1,7 +1,7 @@
 .PHONY:legit
-legit:
+legit:## pushd legit && $(MAKE) legit && popd
 	@pushd legit && $(MAKE) legit && popd
-legit-clean:
+legit-clean:## legit-clean
 	@pushd legit && $(MAKE) clean && popd
 # vim: set noexpandtab:
 # vim: set setfiletype make

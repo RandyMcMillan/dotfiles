3823e95f5049801dca05a084dcae6e97cb088668
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-03 11:30:04 -0500

    make: legit/nostril - check path

--- legit.mk
+++ legit.mk
@@ -1,6 +1,7 @@
 .PHONY:legit
 legit:## pushd legit && $(MAKE) legit && popd
 	@pushd legit && $(MAKE) legit && popd
+	@pushd legit && $(MAKE) cargo-install && popd
 legit-clean:## legit-clean
 	@pushd legit && $(MAKE) clean && popd
 # vim: set noexpandtab:
--- nostril.mk
+++ nostril.mk
@@ -2,5 +2,7 @@
 nostril:## pushd nostril && $(MAKE) nostril && popd
 	@pushd nostril && $(MAKE) nostril && popd
 	@pushd nostril && $(MAKE) nostril install && popd
+nostril-clean:## nostril-clean
+	@pushd nostril && $(MAKE) clean && popd
 # vim: set noexpandtab:
 # vim: set setfiletype make

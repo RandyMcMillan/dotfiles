Lajandra/bitcoin

Make bitcoin-node spawn a bitcoin-wallet process
Spawn wallet subprocess instead of running wallet code internally

src/wallet/init.cpp
@@ -6,6 +6,7 @@
 #include <init.h>
 #include <interfaces/chain.h>
 #include <interfaces/init.h>
+#include <interfaces/ipc.h>
 #include <interfaces/wallet.h>
 #include <net.h>
 #include <node/context.h>
@@ -133,7 +134,24 @@ void WalletInit::Construct(NodeContext& node) const
         LogPrintf("Wallet disabled!\n");
         return;
     }
+<<<<<<< HEAD
     auto wallet_loader = node.init->makeWalletLoader(*node.chain);
     node.wallet_loader = wallet_loader.get();
     node.chain_clients.emplace_back(std::move(wallet_loader));
+||||||| parent of 5ccc7023bd3 (Make bitcoin-node spawn a bitcoin-wallet process)
+    auto wallet_client = node.init->makeWalletClient(*node.chain);
+    node.wallet_client = wallet_client.get();
+    node.chain_clients.emplace_back(std::move(wallet_client));
+=======
+    auto wallet_client = node.init->makeWalletClient(*node.chain);
+    if (!wallet_client) {
+        // If the current process doesn't have wallet support linked in, spawn
+        // a new wallet process.
+        auto init = node.init->ipc()->spawnProcess("bitcoin-wallet");
+        wallet_client = init->makeWalletClient(*node.chain);
+        node.init->ipc()->addCleanup(*wallet_client, [init = init.release()] { delete init; });
+    }
+    node.wallet_client = wallet_client.get();
+    node.chain_clients.emplace_back(std::move(wallet_client));
+>>>>>>> 5ccc7023bd3 (Make bitcoin-node spawn a bitcoin-wallet process)
 }

RandyMcMillan/dotfiles

src/wallet/init.cpp
@@ -6,6 +6,7 @@
 #include <init.h>
 #include <interfaces/chain.h>
 #include <interfaces/init.h>
+#include <interfaces/ipc.h>
 #include <interfaces/wallet.h>
 #include <net.h>
 #include <node/context.h>
@@ -136,6 +137,13 @@ void WalletInit::Construct(NodeContext& node) const
         return;
     }
     auto wallet_loader = node.init->makeWalletLoader(*node.chain);
+    if (!wallet_loader) {
+        // If the current process doesn't have wallet support linked in, spawn
+        // a new wallet process.
+        auto init = node.init->ipc()->spawnProcess("bitcoin-wallet");
+        wallet_loader = init->makeWalletLoader(*node.chain);
+        node.init->ipc()->addCleanup(*wallet_loader, [init = init.release()] { delete init; });
+    }
     node.wallet_loader = wallet_loader.get();
     node.chain_clients.emplace_back(std::move(wallet_loader));
 }
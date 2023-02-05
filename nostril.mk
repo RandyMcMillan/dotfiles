7a6677a3691fd059c662f7ef900ff871b5d9e2f6
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-02-05 11:42:43 -0500

    make: nostril: nostril-clean: update submodule

--- nostril.mk
+++ nostril.mk
@@ -2,5 +2,8 @@
 nostril:
 	@pushd nostril && $(MAKE) nostril && popd
 	@pushd nostril && $(MAKE) nostril install && popd
+nostril-clean:
+	@make clean -C nostril/
+
 # vim: set noexpandtab:
 # vim: set setfiletype make

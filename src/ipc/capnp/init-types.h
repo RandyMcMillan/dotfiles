Lajandra/bitcoin

Make bitcoin-node spawn a bitcoin-wallet process
Spawn wallet subprocess instead of running wallet code internally

src/ipc/capnp/init-types.h
@@ -5,7 +5,31 @@
 #ifndef BITCOIN_IPC_CAPNP_INIT_TYPES_H
 #define BITCOIN_IPC_CAPNP_INIT_TYPES_H
 
+#include <ipc/capnp/chain.capnp.proxy-types.h>
 #include <ipc/capnp/echo.capnp.proxy-types.h>
+#include <ipc/capnp/init.capnp.proxy.h>
 #include <ipc/capnp/node.capnp.proxy-types.h>
+
+namespace mp {
+//! Specialization of makeWalletClient needed because it takes a Chain& reference
+//! argument, not a unique_ptr<Chain> argument, so a manual cleanup
+//! callback is needed to clean up the ProxyServer<messages::Chain> proxy object.
+template <>
+struct ProxyServerMethodTraits<ipc::capnp::messages::Init::MakeWalletClientParams>
+{
+    using Context = ServerContext<ipc::capnp::messages::Init,
+                                  ipc::capnp::messages::Init::MakeWalletClientParams,
+                                  ipc::capnp::messages::Init::MakeWalletClientResults>;
+    static capnp::Void invoke(Context& context);
+};
+
+//! Chain& server-side argument handling. Skips argument so it can
+//! be handled by ProxyServerCustom code.
+template <typename Accessor, typename ServerContext, typename Fn, typename... Args>
+void CustomPassField(TypeList<interfaces::Chain&>, ServerContext& server_context, const Fn& fn, Args&&... args)
+{
+    fn.invoke(server_context, std::forward<Args>(args)...);
+}
+} // namespace mp
 
 #endif // BITCOIN_IPC_CAPNP_INIT_TYPES_H

src/ipc/capnp/init.capnp
@@ -8,16 +8,22 @@ using Cxx = import "/capnp/c++.capnp";
 $Cxx.namespace("ipc::capnp::messages");
 
 using Proxy = import "/mp/proxy.capnp";
+$Proxy.include("interfaces/chain.h");
 $Proxy.include("interfaces/echo.h");
 $Proxy.include("interfaces/init.h");
 $Proxy.include("interfaces/node.h");
 $Proxy.includeTypes("ipc/capnp/init-types.h");
 
+using Chain = import "chain.capnp";
+using Common = import "common.capnp";
 using Echo = import "echo.capnp";
 using Node = import "node.capnp";
+using Wallet = import "wallet.capnp";
 
 interface Init $Proxy.wrap("interfaces::Init") {
     construct @0 (threadMap: Proxy.ThreadMap) -> (threadMap :Proxy.ThreadMap);
     makeEcho @1 (context :Proxy.Context) -> (result :Echo.Echo);
     makeNode @2 (context :Proxy.Context) -> (result :Node.Node);
+    makeChain @3 (context :Proxy.Context) -> (result :Chain.Chain);
+    makeWalletClient @4 (context :Proxy.Context, globalArgs :Common.GlobalArgs, chain :Chain.Chain) -> (result :Wallet.WalletClient);
 }

src/ipc/capnp/init.cpp
@@ -1,0 +1,36 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <capnp/capability.h>
+#include <interfaces/chain.h>
+#include <interfaces/init.h>
+#include <interfaces/ipc.h>
+#include <interfaces/wallet.h>
+#include <ipc/capnp/chain.capnp.h>
+#include <ipc/capnp/chain.capnp.proxy.h>
+#include <ipc/capnp/context.h>
+#include <ipc/capnp/init-types.h>
+#include <ipc/capnp/init.capnp.h>
+#include <ipc/capnp/init.capnp.proxy.h>
+#include <mp/proxy-io.h>
+
+#include <memory>
+#include <utility>
+
+namespace mp {
+template <typename Interface> struct ProxyClient;
+
+::capnp::Void ProxyServerMethodTraits<ipc::capnp::messages::Init::MakeWalletClientParams>::invoke(Context& context)
+{
+    auto params = context.call_context.getParams();
+    auto chain = std::make_unique<ProxyClient<ipc::capnp::messages::Chain>>(
+        params.getChain(), context.proxy_server.m_context.connection, /* destroy_connection= */ false);
+    auto wallet_client = context.proxy_server.m_impl->makeWalletClient(*chain);
+    auto results = context.call_context.getResults();
+    auto result = kj::heap<ProxyServer<ipc::capnp::messages::WalletClient>>(std::shared_ptr<interfaces::WalletClient>(wallet_client.release()), *context.proxy_server.m_context.connection);
+    result->m_context.cleanup.emplace_back([chain = chain.release()] { delete chain; });
+    results.setResult(kj::mv(result));
+    return {};
+}
+} // namespace mp

src/wallet/init.cpp
@@ -6,6 +6,7 @@
 #include <init.h>
 #include <interfaces/chain.h>
 #include <interfaces/init.h>
+#include <interfaces/ipc.h>
 #include <interfaces/wallet.h>
 #include <net.h>
 #include <node/context.h>
@@ -133,7 +134,24 @@ void WalletInit::Construct(NodeContext& node) const
         LogPrintf("Wallet disabled!\n");
         return;
     }
+<<<<<<< HEAD
     auto wallet_loader = node.init->makeWalletLoader(*node.chain);
     node.wallet_loader = wallet_loader.get();
     node.chain_clients.emplace_back(std::move(wallet_loader));
+||||||| parent of 5ccc7023bd3 (Make bitcoin-node spawn a bitcoin-wallet process)
+    auto wallet_client = node.init->makeWalletClient(*node.chain);
+    node.wallet_client = wallet_client.get();
+    node.chain_clients.emplace_back(std::move(wallet_client));
+=======
+    auto wallet_client = node.init->makeWalletClient(*node.chain);
+    if (!wallet_client) {
+        // If the current process doesn't have wallet support linked in, spawn
+        // a new wallet process.
+        auto init = node.init->ipc()->spawnProcess("bitcoin-wallet");
+        wallet_client = init->makeWalletClient(*node.chain);
+        node.init->ipc()->addCleanup(*wallet_client, [init = init.release()] { delete init; });
+    }
+    node.wallet_client = wallet_client.get();
+    node.chain_clients.emplace_back(std::move(wallet_client));
+>>>>>>> 5ccc7023bd3 (Make bitcoin-node spawn a bitcoin-wallet process)
 }

RandyMcMillan/dotfiles

src/ipc/capnp/init-types.h
@@ -5,7 +5,31 @@
 #ifndef BITCOIN_IPC_CAPNP_INIT_TYPES_H
 #define BITCOIN_IPC_CAPNP_INIT_TYPES_H
 
+#include <ipc/capnp/chain.capnp.proxy-types.h>
 #include <ipc/capnp/echo.capnp.proxy-types.h>
+#include <ipc/capnp/init.capnp.proxy.h>
 #include <ipc/capnp/node.capnp.proxy-types.h>
+
+namespace mp {
+//! Specialization of makeWalletLoader needed because it takes a Chain& reference
+//! argument, not a unique_ptr<Chain> argument, so a manual cleanup
+//! callback is needed to clean up the ProxyServer<messages::Chain> proxy object.
+template <>
+struct ProxyServerMethodTraits<ipc::capnp::messages::Init::MakeWalletLoaderParams>
+{
+    using Context = ServerContext<ipc::capnp::messages::Init,
+                                  ipc::capnp::messages::Init::MakeWalletLoaderParams,
+                                  ipc::capnp::messages::Init::MakeWalletLoaderResults>;
+    static capnp::Void invoke(Context& context);
+};
+
+//! Chain& server-side argument handling. Skips argument so it can
+//! be handled by ProxyServerCustom code.
+template <typename Accessor, typename ServerContext, typename Fn, typename... Args>
+void CustomPassField(TypeList<interfaces::Chain&>, ServerContext& server_context, const Fn& fn, Args&&... args)
+{
+    fn.invoke(server_context, std::forward<Args>(args)...);
+}
+} // namespace mp
 
 #endif // BITCOIN_IPC_CAPNP_INIT_TYPES_H

src/ipc/capnp/init.capnp
@@ -8,16 +8,22 @@ using Cxx = import "/capnp/c++.capnp";
 $Cxx.namespace("ipc::capnp::messages");
 
 using Proxy = import "/mp/proxy.capnp";
+$Proxy.include("interfaces/chain.h");
 $Proxy.include("interfaces/echo.h");
 $Proxy.include("interfaces/init.h");
 $Proxy.include("interfaces/node.h");
 $Proxy.includeTypes("ipc/capnp/init-types.h");
 
+using Chain = import "chain.capnp";
+using Common = import "common.capnp";
 using Echo = import "echo.capnp";
 using Node = import "node.capnp";
+using Wallet = import "wallet.capnp";
 
 interface Init $Proxy.wrap("interfaces::Init") {
     construct @0 (threadMap: Proxy.ThreadMap) -> (threadMap :Proxy.ThreadMap);
     makeEcho @1 (context :Proxy.Context) -> (result :Echo.Echo);
     makeNode @2 (context :Proxy.Context) -> (result :Node.Node);
+    makeChain @3 (context :Proxy.Context) -> (result :Chain.Chain);
+    makeWalletLoader @4 (context :Proxy.Context, globalArgs :Common.GlobalArgs, chain :Chain.Chain) -> (result :Wallet.WalletLoader);
 }

src/ipc/capnp/init.cpp
@@ -1,0 +1,36 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <capnp/capability.h>
+#include <interfaces/chain.h>
+#include <interfaces/init.h>
+#include <interfaces/ipc.h>
+#include <interfaces/wallet.h>
+#include <ipc/capnp/chain.capnp.h>
+#include <ipc/capnp/chain.capnp.proxy.h>
+#include <ipc/capnp/context.h>
+#include <ipc/capnp/init-types.h>
+#include <ipc/capnp/init.capnp.h>
+#include <ipc/capnp/init.capnp.proxy.h>
+#include <mp/proxy-io.h>
+
+#include <memory>
+#include <utility>
+
+namespace mp {
+template <typename Interface> struct ProxyClient;
+
+::capnp::Void ProxyServerMethodTraits<ipc::capnp::messages::Init::MakeWalletLoaderParams>::invoke(Context& context)
+{
+    auto params = context.call_context.getParams();
+    auto chain = std::make_unique<ProxyClient<ipc::capnp::messages::Chain>>(
+        params.getChain(), context.proxy_server.m_context.connection, /* destroy_connection= */ false);
+    auto wallet_loader = context.proxy_server.m_impl->makeWalletLoader(*chain);
+    auto results = context.call_context.getResults();
+    auto result = kj::heap<ProxyServer<ipc::capnp::messages::WalletLoader>>(std::shared_ptr<interfaces::WalletLoader>(wallet_loader.release()), *context.proxy_server.m_context.connection);
+    result->m_context.cleanup.emplace_back([chain = chain.release()] { delete chain; });
+    results.setResult(kj::mv(result));
+    return {};
+}
+} // namespace mp

src/wallet/init.cpp
@@ -6,6 +6,7 @@
 #include <init.h>
 #include <interfaces/chain.h>
 #include <interfaces/init.h>
+#include <interfaces/ipc.h>
 #include <interfaces/wallet.h>
 #include <net.h>
 #include <node/context.h>
@@ -133,6 +134,13 @@ void WalletInit::Construct(NodeContext& node) const
         return;
     }
     auto wallet_loader = node.init->makeWalletLoader(*node.chain);
+    if (!wallet_loader) {
+        // If the current process doesn't have wallet support linked in, spawn
+        // a new wallet process.
+        auto init = node.init->ipc()->spawnProcess("bitcoin-wallet");
+        wallet_loader = init->makeWalletLoader(*node.chain);
+        node.init->ipc()->addCleanup(*wallet_loader, [init = init.release()] { delete init; });
+    }
     node.wallet_loader = wallet_loader.get();
     node.chain_clients.emplace_back(std::move(wallet_loader));
 }
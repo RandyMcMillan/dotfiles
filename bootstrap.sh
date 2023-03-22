018f6eec5f35516fd78b2737ddf1e88bac1de813
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-03-21 21:21:09 -0400

    bootstrap.sh: handle force arg

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,9 @@ function doIt() {
 	source $PWD/.bashrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "force" ]; then
+	doIt;
+elif [ "$2" == "force" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;

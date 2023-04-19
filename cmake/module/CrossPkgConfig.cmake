16eef7730dcda252677179c25c89ee296c7c9107
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-04-19 16:13:50 +0100

    cmake: Add cross-compiling support
    To configure CMake for cross-compiling, use
    `--toolchain depends/${HOST}/share/toolchain.cmake` command-line option.

---
+++ cmake/module/CrossPkgConfig.cmake
@@ -0,0 +1,19 @@
+# Copyright (c) 2023 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+find_package(PkgConfig REQUIRED)
+
+macro(cross_pkg_check_modules)
+  if(CMAKE_CROSSCOMPILING)
+    set(pkg_config_path_saved "$ENV{PKG_CONFIG_PATH}")
+    set(pkg_config_libdir_saved "$ENV{PKG_CONFIG_LIBDIR}")
+    set(ENV{PKG_CONFIG_PATH} ${PKG_CONFIG_PATH})
+    set(ENV{PKG_CONFIG_LIBDIR} ${PKG_CONFIG_LIBDIR})
+    pkg_check_modules(${ARGV})
+    set(ENV{PKG_CONFIG_PATH} ${pkg_config_path_saved})
+    set(ENV{PKG_CONFIG_LIBDIR} ${pkg_config_libdir_saved})
+  else()
+    pkg_check_modules(${ARGV})
+  endif()
+endmacro()

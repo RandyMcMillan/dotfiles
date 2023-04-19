bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

ci: Migrate CI tasks to CMake

ci/test/00_setup_env_win64.sh
@@ -13,4 +13,5 @@ export DPKG_ADD_ARCH="i386"
 export PACKAGES="python3 nsis g++-mingw-w64-x86-64-posix wine-binfmt wine64 wine32 file"
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-export BITCOIN_CONFIG="--enable-reduce-exports --enable-external-signer --disable-gui-tests"
+export BITCOIN_CONFIG="-DREDUCE_EXPORTS=ON -DWITH_EXTERNAL_SIGNER=OFF"
+export TESTJOBS="-j4"

RandyMcMillan/dotfiles

ci/test/00_setup_env_win64.sh
@@ -13,4 +13,5 @@ export DPKG_ADD_ARCH="i386"
 export PACKAGES="python3 nsis g++-mingw-w64-x86-64-posix wine-binfmt wine64 wine32 file"
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-export BITCOIN_CONFIG="--enable-reduce-exports --enable-external-signer --disable-gui-tests"
+export BITCOIN_CONFIG="-DREDUCE_EXPORTS=ON -DWITH_EXTERNAL_SIGNER=ON"
+export TESTJOBS="-j4"
4169b3d6d6d3d640b8ab6cbc43b3353215ecd5a8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-17 14:28:15 -0400

    make: nostril: update submodule

--- nostril.mk
+++ nostril.mk
@@ -1,5 +1,6 @@
 .PHONY:nostril
 nostril:
+	@git submodule update --init --reference $@ --recursive
 	@pushd nostril && $(MAKE) nostril && popd
 	@pushd nostril && $(MAKE) nostril install && popd
 nostril-clean:

2560a650241c8409e56556482b0c760378836c45
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-14 10:16:48 -0400

    act.mk: help formatting

--- act.mk
+++ act.mk
@@ -1,19 +1,19 @@
 #NOTE: using -C for container context
 #gpg-recv-keys-bitcoin-devs:##
 #	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD)/.github -vr -W $(PWD)/.github/workflows/$@.yml
-macos-matrix:docker-start##
+macos-matrix:docker-start## 	macos-matrix
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
-make-nvm:docker-start##
+make-nvm:docker-start## 	make-nvm
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
-ubuntu-make-legit:docker-start##
+ubuntu-make-legit:docker-start## 	ubuntu-make-legit
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
-ubuntu-pre-release:docker-start##
+ubuntu-pre-release:docker-start## 	ubuntu-pre-release
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
-macos-make-legit:docker-start##
+macos-make-legit:docker-start## 	macos-make-legit
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
-macos-pre-release:docker-start##
+macos-pre-release:docker-start## 	macos-pre-release
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml
-makefile:docker-start##
+makefile:docker-start## 	makefile
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vb  -W $(PWD)/.github/workflows/$@.yml
-ubuntu-matrix:docker-start##
+ubuntu-matrix:docker-start## 	ubuntu-matrix
 	@export $(cat ~/GH_TOKEN.txt) && act -C $(PWD) -vbr -W $(PWD)/.github/workflows/$@.yml

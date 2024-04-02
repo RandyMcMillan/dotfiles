d5efab3625c7281c1a780824a39592bb6fea31af
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-04-02 16:35:45 -0400

    nostril.mk:make nostril

--- nostril.mk
+++ nostril.mk
@@ -1,10 +1,8 @@
 .PHONY:nostril
 nostril:
-	@git submodule update --init --reference $@ --recursive
-	@pushd nostril && $(MAKE) nostril && popd
-	#@pushd nostril && $(MAKE) nostril install && popd
+	@pushd nostril && $(MAKE) install && popd
 nostril-clean:
-	@make clean -C nostril/
+	@$(MAKE) clean -C nostril/
 
 # vim: set noexpandtab:
 # vim: set setfiletype make

925846bf2e0bb6ba2db0daac683a84f091a8ee7d
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-07-07 13:18:15 -0400

    nostril.mk:make nostril

--- nostril.mk
+++ nostril.mk
@@ -1,6 +1,9 @@
 .PHONY:nostril
 nostril:
-	@pushd nostril && $(MAKE) install && popd
+	@pushd nostril && $(MAKE) clean all && popd
+	@mkdir -p /usr/local/bin
+	install nostril/nostril /usr/local/bin/ && \
+	$(shell which nostril)
 nostril-clean:
 	@rm nostril/nostril
 

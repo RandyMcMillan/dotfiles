5ca38d31ab22543bdfbfc1d9d2c73892fe7fdfb8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-07-07 13:35:55 -0400

    make:cargo-binstall

--- cargo.mk
+++ cargo.mk
@@ -1,3 +1,11 @@
+cargo-binstall:## 	cargo-binstall
+	cargo install cargo-quickinstall && \
+	cargo-quickinstall cargo-binstall
+cargo-binstall-gnostr-bins:## 	cargo-binstall-gnostr-bins
+	cargo install cargo-quickinstall && \
+	cargo-quickinstall gnostr-bins || \
+	brew tap gnostr-org/homebrew-gnostr-org && \
+	brew install gnostr-bins
 cargo-install-all:
 	for p in $(shell cargo search gnostr -q --limit 100 | cut -d "=" -f1); do cargo install $$p;done
 	cargo install cargo-sweep

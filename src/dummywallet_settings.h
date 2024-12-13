c12eba219182b02042a22ca84e365e2be3e9ad37
Ryan Ofsky <ryan@ofsky.org>
2024-12-13 17:53:24 -0500

    dummywallet_settings.h: Add DiscardFeeSettingHidden

--- src/dummywallet_settings.h
+++ src/dummywallet_settings.h
@@ -30,4 +30,8 @@ using DisableWalletSettingHidden = common::Setting<
     "-disablewallet", common::Unset, common::SettingOptions{.legacy = true}>
     ::Category<OptionsCategory::HIDDEN>;
 
+using DiscardFeeSettingHidden = common::Setting<
+    "-discardfee=<amt>", common::Unset, common::SettingOptions{.legacy = true}>
+    ::Category<OptionsCategory::HIDDEN>;
+
 #endif // BITCOIN_DUMMYWALLET_SETTINGS_H

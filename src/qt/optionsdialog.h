f66f5b2c56b915834bc9214d73f067e05fa62307
Luke Dashjr <luke-jr+git@utopios.org>
2025-07-14 23:51:04 +0000

    GUI/Options: Configure maxtxlegacysigops using settings

--- src/qt/optionsdialog.h
+++ src/qt/optionsdialog.h
@@ -142,6 +142,7 @@ private:
     QCheckBox *rejectbarepubkey;
     QCheckBox *rejectbaremultisig;
     QSpinBox *maxscriptsize;
+    QSpinBox *maxtxlegacysigops;
     QSpinBox *datacarriersize;
     QDoubleSpinBox *datacarriercost;
     QCheckBox *rejectnonstddatacarrier;

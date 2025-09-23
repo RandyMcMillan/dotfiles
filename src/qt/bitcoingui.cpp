c76abf4e7533cb3d23128356361d4b0a5e25b976
randymcmillan <randymcmillan@protonmail.com>
2025-09-23 12:52:58 -0400

    src/qt/bitcoingui.cpp:testnet4

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -540,7 +540,7 @@ void BitcoinGUI::createMenuBar()
 
     connect(chain_selection_menu, &QMenu::aboutToShow, [this, chain_selection_menu] {
         chain_selection_menu->clear();
-        const std::vector<std::pair<QString, QString>> chains = {{"main", "&Bitcoin"}, {"test", "&Testnet"}, {"regtest", "&Regtest"}, {"signet", "&Signet"}};
+        const std::vector<std::pair<QString, QString>> chains = {{"main", "&Bitcoin"}, {"testnet4", "&Testnet4"}, {"regtest", "&Regtest"}, {"signet", "&Signet"}};
         const std::string current_chain = Params().GetChainTypeString();
         for (const auto& chain : chains) {
         const bool is_current = current_chain == chain.first.toStdString();

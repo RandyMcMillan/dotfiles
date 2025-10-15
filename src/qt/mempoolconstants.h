4c20cf1a68471bfa9ef42062a454c9d9764f323d
randymcmillan <randymcmillan@protonmail.com>
2025-10-15 14:21:36 -0400

    more stats_graph styling

--- src/qt/mempoolconstants.h
+++ src/qt/mempoolconstants.h
@@ -1,32 +1,32 @@
 #include <QColor>
 
-static const char    *LABEL_FONT                 = "Roboto Mono";
-static int           LABEL_TITLE_SIZE            = 22;
-static int           LABEL_KV_SIZE               = 12;
+static const char* LABEL_FONT = "Roboto Mono";
+static int LABEL_TITLE_SIZE = 22;
+static int LABEL_KV_SIZE = 12;
 
-static const int     LABEL_LEFT_SIZE             = 0;// space + #.# MvB -- //
-static const int     LABEL_RIGHT_SIZE            = 0;
-static const int     GRAPH_PADDING_LEFT          = 100+LABEL_LEFT_SIZE;
-static const int     GRAPH_PADDING_RIGHT         = 50 +LABEL_RIGHT_SIZE;
-static const int     GRAPH_PADDING_TOP           = 50;
-static const int     GRAPH_PADDING_TOP_LABEL     = 0;
-static const int     GRAPH_PADDING_BOTTOM        = 50;
+static const int LABEL_LEFT_SIZE = 60; // space + #.# MvB -- //
+static const int LABEL_RIGHT_SIZE = 0;
+static const int GRAPH_PADDING_LEFT = 10 + LABEL_LEFT_SIZE;
+static const int GRAPH_PADDING_RIGHT = 10 + LABEL_RIGHT_SIZE;
+static const int GRAPH_PADDING_TOP = 10;
+static const int GRAPH_PADDING_TOP_LABEL = 0;
+static const int GRAPH_PADDING_BOTTOM = 0;
 
-static const int     ITEM_TX_COUNT_PADDING_LEFT  = 5;
-static const int     ITEM_TX_COUNT_PADDING_RIGHT = 5;
-static const int     AMOUNT_OF_H_LINES           = 9;
-static const double  GRAPH_PATH_SCALAR           = 1.0;
+static const int ITEM_TX_COUNT_PADDING_LEFT = 5;
+static const int ITEM_TX_COUNT_PADDING_RIGHT = 5;
+static const int AMOUNT_OF_H_LINES = 9;
+static const double GRAPH_PATH_SCALAR = 1.0;
 
-const qreal C_X                                  = 10;
-const qreal C_W                                  = 20;
-const qreal C_H                                  = 20;
-const qreal C_MARGIN                             = 2;
+const qreal C_X = 10;
+const qreal C_W = 20;
+const qreal C_H = 20;
+const qreal C_MARGIN = 2;
 
-bool const ADD_TEXT                              = true;
-bool const ADD_FEE_RANGES                        = true;
-bool const ADD_FEE_RECTS                         = true;
-bool const MEMPOOL_GRAPH_LOGGING                 = true;
-bool const ADD_TOTAL_TEXT                        = true;
+bool const ADD_TEXT = true;
+bool const ADD_FEE_RANGES = true;
+bool const ADD_FEE_RECTS = true;
+bool const MEMPOOL_GRAPH_LOGGING = true;
+bool const ADD_TOTAL_TEXT = true;
 
 const static std::vector<QColor> colors = {
     // Hue ranges from 120 (Green) down to 0 (Red)
@@ -54,8 +54,8 @@ const static std::vector<QColor> colors = {
     QColor::fromHsv(15, 255, 255),
     QColor::fromHsv(10, 255, 255),
     QColor::fromHsv(5, 255, 255),
-    QColor::fromHsv(0, 255, 255),  // Red
-    QColor::fromHsv(0, 255, 200),  // Darker Red
-    QColor::fromHsv(0, 255, 150),  // Even Darker Red
-    QColor(70, 70, 70, 255)        // Final dark gray/black for extreme values
+    QColor::fromHsv(0, 255, 255), // Red
+    QColor::fromHsv(0, 255, 200), // Darker Red
+    QColor::fromHsv(0, 255, 150), // Even Darker Red
+    QColor(70, 70, 70, 255)       // Final dark gray/black for extreme values
 };

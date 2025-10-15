17209769df171ff93da322b913b2d569ee0b6e63
randymcmillan <randymcmillan@protonmail.com>
2025-10-15 09:01:05 -0400

    src/qt/mempoolconstants.h:colors based on Aesthetically Pleasing HSV-Based Fee Scale

--- src/qt/mempoolconstants.h
+++ src/qt/mempoolconstants.h
@@ -29,59 +29,33 @@ bool const MEMPOOL_GRAPH_LOGGING                 = true;
 bool const ADD_TOTAL_TEXT                        = true;
 
 const static std::vector<QColor> colors = {
-QColor(100, 149, 237, 255), // Cornflower Blue (very low fee)
-QColor(65, 105, 225, 255),  // Royal Blue
-QColor(0, 0, 205, 255),     // Medium Blue
-QColor(0, 191, 255, 255),   // Deep Sky Blue
-QColor(0, 128, 128, 255),   // Teal
-QColor(0, 139, 139, 255),   // Dark Cyan
-QColor(0, 255, 127, 255),   // Spring Green
-QColor(50, 205, 50, 255),   // Lime Green
-QColor(34, 139, 34, 255),   // Forest Green
-QColor(124, 252, 0, 255),   // Lawn Green
-QColor(173, 255, 47, 255),  // Green Yellow
-QColor(255, 255, 0, 255),   // Yellow
-QColor(255, 215, 0, 255),   // Gold
-QColor(255, 165, 0, 255),   // Orange
-QColor(255, 140, 0, 255),   // Dark Orange
-QColor(255, 99, 71, 255),   // Tomato
-QColor(255, 69, 0, 255),    // Orange Red
-QColor(255, 0, 0, 255),     // Red
-QColor(220, 20, 60, 255),   // Crimson
-QColor(178, 34, 34, 255),   // Firebrick
-QColor(139, 0, 0, 255),     // Dark Red
-QColor(128, 0, 0, 255),     // Maroon
-QColor(160, 82, 45, 255),   // Sienna
-QColor(139, 69, 19, 255),   // Saddle Brown
-QColor(105, 105, 105, 255), // Dim Gray
-QColor(70, 130, 180, 255),  // Steel Blue
-QColor(112, 128, 144, 255), // Slate Gray
-QColor(47, 79, 79, 255),    // Dark Slate Gray (very high fee, almost black)
-QColor(112, 128, 144, 255), // Slate Gray
-QColor(70, 130, 180, 255),  // Steel Blue
-QColor(105, 105, 105, 255), // Dim Gray
-QColor(139, 69, 19, 255),   // Saddle Brown
-QColor(160, 82, 45, 255),   // Sienna
-QColor(128, 0, 0, 255),     // Maroon
-QColor(139, 0, 0, 255),     // Dark Red
-QColor(178, 34, 34, 255),   // Firebrick
-QColor(220, 20, 60, 255),   // Crimson
-QColor(255, 0, 0, 255),     // Red
-QColor(255, 69, 0, 255),    // Orange Red
-QColor(255, 99, 71, 255),   // Tomato
-QColor(255, 140, 0, 255),   // Dark Orange
-QColor(255, 165, 0, 255),   // Orange
-QColor(255, 215, 0, 255),   // Gold
-QColor(255, 255, 0, 255),   // Yellow
-QColor(173, 255, 47, 255),  // Green Yellow
-QColor(124, 252, 0, 255),   // Lawn Green
-QColor(34, 139, 34, 255),   // Forest Green
-QColor(50, 205, 50, 255),   // Lime Green
-QColor(0, 255, 127, 255),   // Spring Green
-QColor(0, 139, 139, 255),   // Dark Cyan
-QColor(0, 128, 128, 255),   // Teal
-QColor(0, 191, 255, 255),   // Deep Sky Blue
-QColor(0, 0, 205, 255),     // Medium Blue
-QColor(65, 105, 225, 255),  // Royal Blue
-QColor(100, 149, 237, 255), // Cornflower Blue (very low fee)
+    // Hue ranges from 120 (Green) down to 0 (Red)
+    QColor::fromHsv(120, 255, 255), // Bright Green
+    QColor::fromHsv(115, 255, 255),
+    QColor::fromHsv(110, 255, 255),
+    QColor::fromHsv(105, 255, 255),
+    QColor::fromHsv(100, 255, 255),
+    QColor::fromHsv(95, 255, 255),
+    QColor::fromHsv(90, 255, 255),
+    QColor::fromHsv(85, 255, 255),
+    QColor::fromHsv(80, 255, 255),
+    QColor::fromHsv(75, 255, 255), // Lime/Yellow-Green
+    QColor::fromHsv(70, 255, 255),
+    QColor::fromHsv(65, 255, 255),
+    QColor::fromHsv(60, 255, 255), // Yellow
+    QColor::fromHsv(55, 255, 255),
+    QColor::fromHsv(50, 255, 255),
+    QColor::fromHsv(45, 255, 255),
+    QColor::fromHsv(40, 255, 255), // Yellow-Orange
+    QColor::fromHsv(35, 255, 255),
+    QColor::fromHsv(30, 255, 255),
+    QColor::fromHsv(25, 255, 255),
+    QColor::fromHsv(20, 255, 255), // Orange
+    QColor::fromHsv(15, 255, 255),
+    QColor::fromHsv(10, 255, 255),
+    QColor::fromHsv(5, 255, 255),
+    QColor::fromHsv(0, 255, 255),  // Red
+    QColor::fromHsv(0, 255, 200),  // Darker Red
+    QColor::fromHsv(0, 255, 150),  // Even Darker Red
+    QColor(70, 70, 70, 255)        // Final dark gray/black for extreme values
 };

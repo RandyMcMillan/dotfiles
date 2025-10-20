8d372599dcf5bb81ed40a3d3c58c5992ba9ae91f
Luke Dashjr <luke-jr+git@utopios.org>
2025-10-20 00:28:40 +0000

    Add questionmark to end of BIP148 service bit string, and add to bitcoin-cli

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -483,6 +483,8 @@ private:
                 str += 'T';
             } else if (s == "UTREEXO_TMP?") {
                 str += 'y';
+            } else if (s == "BIP148?") {
+                str += '1';
             } else {
                 str += ToLower(s[0]);
             }
@@ -765,6 +767,7 @@ public:
         "           \"T\" - UTREEXO_ARCHIVE peer can handle Utreexo proof requests for all  historical blocks\n"
         "           \"y\" - UTREEXO_TMP? peer can handle Utreexo proof requests\n"
         "           \"r\" - REPLACE_BY_FEE? peer supports replacement of transactions without BIP 125 signalling\n"
+        "           \"1\" - BIP148? peer enforces the BIP148 User-Activated SoftFork\n"
         "           \"m\" - MALICIOUS? peer openly seeks to aid in bypassing network policy/spam filters (OR to sabotage nodes that seek to)\n"
         "           \"u\" - UNKNOWN: unrecognized bit flag\n"
         "  v        Version of transport protocol used for the connection\n"

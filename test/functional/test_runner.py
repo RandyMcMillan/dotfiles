ec403fd6c52f6b70e382c4e263caa479abcb5d60
3c853b6299 <3c853b6299@pm.me>
2025-11-04 18:18:45 -0600

    test: implement functional tests for ReducedData Spec

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -97,6 +97,7 @@ BASE_SCRIPTS = [
     'feature_taproot.py',
     'feature_reduced_data_temporary_deployment.py',
     'feature_bip9_max_activation_height.py',
+    'feature_rdts.py',
     'feature_block.py',
     'mempool_ephemeral_dust.py',
     'wallet_conflicts.py --legacy-wallet',

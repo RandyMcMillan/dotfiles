8c0313fa95a1a994587751729d4ee3d9e8298d9b
Léo Haf <leo@haf.ovh>
2025-12-14 14:06:10 +0100

    test: Fix fuzz for miniscript.cpp

--- src/test/fuzz/miniscript.cpp
+++ src/test/fuzz/miniscript.cpp
@@ -1132,13 +1132,25 @@ void TestNode(const MsCtx script_ctx, const NodeRef& node, FuzzedDataProvider& p
         SatisfactionToWitness(script_ctx, witness_nonmal, script, builder);
         ScriptError serror;
         bool res = VerifyScript(DUMMY_SCRIPTSIG, script_pubkey, &witness_nonmal, STANDARD_SCRIPT_VERIFY_FLAGS, CHECKER_CTX, &serror);
-        // Non-malleable satisfactions are guaranteed to be valid if ValidSatisfactions().
-        if (node->ValidSatisfactions()) assert(res);
+        // Non-malleable satisfactions are guaranteed to be valid if ValidSatisfactions(), unless REDUCED_DATA rules are violated.
+        if (node->ValidSatisfactions()) {
+            assert(res ||
+                   serror == ScriptError::SCRIPT_ERR_PUSH_SIZE ||
+                   serror == ScriptError::SCRIPT_ERR_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM ||
+                   serror == ScriptError::SCRIPT_ERR_DISCOURAGE_UPGRADABLE_TAPROOT_VERSION ||
+                   serror == ScriptError::SCRIPT_ERR_DISCOURAGE_OP_SUCCESS ||
+                   serror == ScriptError::SCRIPT_ERR_TAPSCRIPT_MINIMALIF);
+        }
         // More detailed: non-malleable satisfactions must be valid, or could fail with ops count error (if CheckOpsLimit failed),
-        // or with a stack size error (if CheckStackSize check failed).
+        // or with a stack size error (if CheckStackSize check failed), or with REDUCED_DATA-related errors.
         assert(res ||
                (!node->CheckOpsLimit() && serror == ScriptError::SCRIPT_ERR_OP_COUNT) ||
-               (!node->CheckStackSize() && serror == ScriptError::SCRIPT_ERR_STACK_SIZE));
+               (!node->CheckStackSize() && serror == ScriptError::SCRIPT_ERR_STACK_SIZE) ||
+               serror == ScriptError::SCRIPT_ERR_PUSH_SIZE ||
+               serror == ScriptError::SCRIPT_ERR_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM ||
+               serror == ScriptError::SCRIPT_ERR_DISCOURAGE_UPGRADABLE_TAPROOT_VERSION ||
+               serror == ScriptError::SCRIPT_ERR_DISCOURAGE_OP_SUCCESS ||
+               serror == ScriptError::SCRIPT_ERR_TAPSCRIPT_MINIMALIF);
     }
 
     if (mal_success && (!nonmal_success || witness_mal.stack != witness_nonmal.stack)) {
@@ -1148,8 +1160,15 @@ void TestNode(const MsCtx script_ctx, const NodeRef& node, FuzzedDataProvider& p
         ScriptError serror;
         bool res = VerifyScript(DUMMY_SCRIPTSIG, script_pubkey, &witness_mal, STANDARD_SCRIPT_VERIFY_FLAGS, CHECKER_CTX, &serror);
         // Malleable satisfactions are not guaranteed to be valid under any conditions, but they can only
-        // fail due to stack or ops limits.
-        assert(res || serror == ScriptError::SCRIPT_ERR_OP_COUNT || serror == ScriptError::SCRIPT_ERR_STACK_SIZE);
+        // fail due to stack or ops limits, or REDUCED_DATA-related errors.
+        assert(res ||
+               serror == ScriptError::SCRIPT_ERR_OP_COUNT ||
+               serror == ScriptError::SCRIPT_ERR_STACK_SIZE ||
+               serror == ScriptError::SCRIPT_ERR_PUSH_SIZE ||
+               serror == ScriptError::SCRIPT_ERR_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM ||
+               serror == ScriptError::SCRIPT_ERR_DISCOURAGE_UPGRADABLE_TAPROOT_VERSION ||
+               serror == ScriptError::SCRIPT_ERR_DISCOURAGE_OP_SUCCESS ||
+               serror == ScriptError::SCRIPT_ERR_TAPSCRIPT_MINIMALIF);
     }
 
     if (node->IsSane()) {

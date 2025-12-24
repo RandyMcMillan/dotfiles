b47322e34e47c806e666867acc67ed601fd35849
Dathon Ohm <dathonohm@proton.me>
2025-12-24 14:21:23 -0600

    fixup! Add expiry support to versionbit deployments

--- src/deploymentstatus.h
+++ src/deploymentstatus.h
@@ -41,14 +41,7 @@ inline bool DeploymentActiveAt(const CBlockIndex& index, const Consensus::Params
 inline bool DeploymentActiveAt(const CBlockIndex& index, const Consensus::Params& params, Consensus::DeploymentPos dep, VersionBitsCache& versionbitscache)
 {
     assert(Consensus::ValidDeployment(dep));
-    if (ThresholdState::ACTIVE != versionbitscache.State(index.pprev, params, dep)) return false;
-
-    const auto& deployment = params.vDeployments[dep];
-    // Permanent deployment (never expires)
-    if (deployment.active_duration == std::numeric_limits<int>::max()) return true;
-
-    const int activation_height = versionbitscache.StateSinceHeight(index.pprev, params, dep);
-    return index.nHeight < activation_height + deployment.active_duration;
+    return DeploymentActiveAfter(index.pprev, params, dep, versionbitscache);
 }
 
 /** Determine if a deployment is enabled (can ever be active) */

5a54af716cd8d4ad25bf1025fa8df0455a90dcd3
Léo Haf <leo@haf.ovh>
2025-12-31 12:32:49 +0100

    net: ask DNS seed for x8000009

--- src/protocol.h
+++ src/protocol.h
@@ -366,7 +366,7 @@ std::vector<std::string> serviceFlagsToStr(uint64_t flags);
  * should be updated appropriately to filter for nodes with
  * desired service flags (compatible with our new flags).
  */
-constexpr ServiceFlags SeedsServiceFlags() { return ServiceFlags(NODE_NETWORK | NODE_WITNESS); }
+constexpr ServiceFlags SeedsServiceFlags() { return ServiceFlags(NODE_NETWORK | NODE_WITNESS | NODE_UASF_REDUCED_DATA); }
 
 /**
  * Checks if a peer with the given service flags may be capable of having a

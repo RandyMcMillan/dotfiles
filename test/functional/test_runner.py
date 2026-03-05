Rob1Ham/bitcoin

Support regtest vbparams for max_activation_height and active_duration

test/functional/test_runner.py
@@ -95,6 +95,8 @@ BASE_SCRIPTS = [
     # vv Tests less than 5m vv
     'feature_fee_estimation.py',
     'feature_taproot.py',
+    'feature_reduced_data_temporary_deployment.py',
+    'feature_uasf_max_activation_height.py',
     'feature_block.py',
     'mempool_ephemeral_dust.py',
     'wallet_conflicts.py --legacy-wallet',

RandyMcMillan/dotfiles

test/functional/test_runner.py
@@ -95,6 +95,8 @@ BASE_SCRIPTS = [
     # vv Tests less than 5m vv
     'feature_fee_estimation.py',
     'feature_taproot.py',
+    'feature_reduced_data_temporary_deployment.py',
+    'feature_bip9_max_activation_height.py',
     'feature_block.py',
     'mempool_ephemeral_dust.py',
     'wallet_conflicts.py --legacy-wallet',
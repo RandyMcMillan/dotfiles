e1784c23ab0ae85657aa2f96987e0ad539467187
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2026-04-20 08:59:17 -0400

    scripts/install-nvm.sh

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
